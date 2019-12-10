fcad5539a1cf1c3d3c042f7db0e4ab6bc450bd30
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2019-12-10 12:28:56 +0530

    Output to commonjs format

--- dist/Experiment/enums.js
+++ dist/Experiment/enums.js
@@ -1,20 +1,22 @@
-export var CONSISTENCY_RULE;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CONSISTENCY_RULE;
 (function (CONSISTENCY_RULE) {
     CONSISTENCY_RULE["INDIVIDUAL"] = "individual";
     CONSISTENCY_RULE["EXPERIMENT"] = "experiment";
     CONSISTENCY_RULE["GROUP"] = "group";
-})(CONSISTENCY_RULE || (CONSISTENCY_RULE = {}));
-export var ASSIGNMENT_UNIT;
+})(CONSISTENCY_RULE = exports.CONSISTENCY_RULE || (exports.CONSISTENCY_RULE = {}));
+var ASSIGNMENT_UNIT;
 (function (ASSIGNMENT_UNIT) {
     ASSIGNMENT_UNIT["INDIVIDUAL"] = "individual";
     ASSIGNMENT_UNIT["GROUP"] = "group";
-})(ASSIGNMENT_UNIT || (ASSIGNMENT_UNIT = {}));
-export var POST_EXPERIMENT_RULE;
+})(ASSIGNMENT_UNIT = exports.ASSIGNMENT_UNIT || (exports.ASSIGNMENT_UNIT = {}));
+var POST_EXPERIMENT_RULE;
 (function (POST_EXPERIMENT_RULE) {
     POST_EXPERIMENT_RULE["CONTINUE"] = "continue";
     POST_EXPERIMENT_RULE["REVERT_TO_DEFAULT"] = "revertToDefault";
-})(POST_EXPERIMENT_RULE || (POST_EXPERIMENT_RULE = {}));
-export var EXPERIMENT_STATE;
+})(POST_EXPERIMENT_RULE = exports.POST_EXPERIMENT_RULE || (exports.POST_EXPERIMENT_RULE = {}));
+var EXPERIMENT_STATE;
 (function (EXPERIMENT_STATE) {
     EXPERIMENT_STATE["INACTIVE"] = "inactive";
     EXPERIMENT_STATE["DEMO"] = "demo";
@@ -22,5 +24,5 @@ export var EXPERIMENT_STATE;
     EXPERIMENT_STATE["ENROLLING"] = "enrolling";
     EXPERIMENT_STATE["ENROLLMENT_COMPLETE"] = "enrollmentComplete";
     EXPERIMENT_STATE["CANCELLED"] = "cancelled";
-})(EXPERIMENT_STATE || (EXPERIMENT_STATE = {}));
+})(EXPERIMENT_STATE = exports.EXPERIMENT_STATE || (exports.EXPERIMENT_STATE = {}));
 //# sourceMappingURL=enums.js.map
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,8 @@
-export { CONSISTENCY_RULE, ASSIGNMENT_UNIT, POST_EXPERIMENT_RULE, EXPERIMENT_STATE } from './Experiment/enums';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var enums_1 = require("./Experiment/enums");
+exports.CONSISTENCY_RULE = enums_1.CONSISTENCY_RULE;
+exports.ASSIGNMENT_UNIT = enums_1.ASSIGNMENT_UNIT;
+exports.POST_EXPERIMENT_RULE = enums_1.POST_EXPERIMENT_RULE;
+exports.EXPERIMENT_STATE = enums_1.EXPERIMENT_STATE;
 //# sourceMappingURL=index.js.map
