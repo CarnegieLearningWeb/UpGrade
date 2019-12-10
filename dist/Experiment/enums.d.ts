619a00c60f42eb3785c70da93e2d9f35d96f70f3
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2019-12-10 11:50:49 +0530

    Generating type definition in dist file

---
+++ dist/Experiment/enums.d.ts
@@ -0,0 +1,21 @@
+export declare enum CONSISTENCY_RULE {
+    INDIVIDUAL = "individual",
+    EXPERIMENT = "experiment",
+    GROUP = "group"
+}
+export declare enum ASSIGNMENT_UNIT {
+    INDIVIDUAL = "individual",
+    GROUP = "group"
+}
+export declare enum POST_EXPERIMENT_RULE {
+    CONTINUE = "continue",
+    REVERT_TO_DEFAULT = "revertToDefault"
+}
+export declare enum EXPERIMENT_STATE {
+    INACTIVE = "inactive",
+    DEMO = "demo",
+    SCHEDULED = "scheduled",
+    ENROLLING = "enrolling",
+    ENROLLMENT_COMPLETE = "enrollmentComplete",
+    CANCELLED = "cancelled"
+}
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,1 @@
 export { CONSISTENCY_RULE, ASSIGNMENT_UNIT, POST_EXPERIMENT_RULE, EXPERIMENT_STATE } from './Experiment/enums';
-//# sourceMappingURL=index.js.map
