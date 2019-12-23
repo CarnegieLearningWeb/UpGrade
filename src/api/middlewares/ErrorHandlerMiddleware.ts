5d1803051194b7707405a99d6cf7aed5557a5cb0
dhavalocked <pdhaval1994.dp@gmail.com>
2019-12-23 17:17:50 +0530

    added errors to DB

--- src/api/middlewares/ErrorHandlerMiddleware.ts
+++ src/api/middlewares/ErrorHandlerMiddleware.ts
@@ -4,21 +4,38 @@ import { ExpressErrorMiddlewareInterface, HttpError, Middleware } from 'routing-
 import { Logger, LoggerInterface } from '../../decorators/Logger';
 import { env } from '../../env';
 import { formatBadReqErrorMessage } from '../../lib/env/utils';
+import { ErrorService } from '../services/ErrorService';
+import { ExperimentError } from '../models/ExperimentError';
 
 @Middleware({ type: 'after' })
 export class ErrorHandlerMiddleware implements ExpressErrorMiddlewareInterface {
   public isProduction = env.isProduction;
 
-  constructor(@Logger(__filename) private log: LoggerInterface) { }
+  constructor(
+    @Logger(__filename) private log: LoggerInterface,
+    public errorService: ErrorService
+  ) { }
 
   public error(error: HttpError, req: express.Request, res: express.Response, next: express.NextFunction): void {
     // It seems like some decorators handle setting the response (i.e. class-validators)
+
+    const message = error.httpCode === 400 ? formatBadReqErrorMessage(error[`errors`]) : error.message;
+
+    const experimentError = new ExperimentError();
+
+    experimentError.name = error.name;
+    experimentError.message = message;
+    experimentError.endPoint = req.originalUrl;
+    experimentError.errorCode = error.httpCode;
+
+    this.errorService.create(experimentError);
+
     if (!res.headersSent) {
       res.status(error.httpCode || 500);
 
       res.json({
         name: error.name,
-        message: error.httpCode === 400 ? formatBadReqErrorMessage(error[`errors`]) : error.message,
+        message: `${message}`,
         errors: error[`errors`] || [],
       });
     }
