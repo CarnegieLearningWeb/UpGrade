b9d29a71de3ed0ba0805a58e30e1df91d6e24dc9
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2019-12-26 15:03:21 +0530

    Making single statement for export

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,3 @@
-export { CONSISTENCY_RULE, ASSIGNMENT_UNIT, POST_EXPERIMENT_RULE, EXPERIMENT_STATE, SERVER_ERROR } from './Experiment/enums';
-export { IEnrollmentCompleteCondition } from './Experiment/interfaces';
+import { CONSISTENCY_RULE, ASSIGNMENT_UNIT, POST_EXPERIMENT_RULE, EXPERIMENT_STATE, SERVER_ERROR } from './Experiment/enums';
+import { IEnrollmentCompleteCondition } from './Experiment/interfaces';
+export { IEnrollmentCompleteCondition, CONSISTENCY_RULE, ASSIGNMENT_UNIT, POST_EXPERIMENT_RULE, EXPERIMENT_STATE, SERVER_ERROR, };
