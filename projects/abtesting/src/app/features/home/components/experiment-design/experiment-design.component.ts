869ac0ea490a58719a77d7bed6d032cd0259017e
Jaydip Hirapara <jaydip.hirapara@playpowerlabs.com>
2020-01-20 10:23:42 +0530

    [Experiment] Fix indentation issue

--- projects/abtesting/src/app/features/home/components/experiment-design/experiment-design.component.ts
+++ projects/abtesting/src/app/features/home/components/experiment-design/experiment-design.component.ts
@@ -8,20 +8,9 @@ import {
   ViewChild,
   ElementRef
 } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  FormArray,
-  AbstractControl
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, AbstractControl } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
-import {
-  NewExperimentDialogEvents,
-  NewExperimentDialogData,
-  NewExperimentPaths,
-  Experiment
-} from '../../../../core/experiments/store/experiments.model';
+import { NewExperimentDialogEvents, NewExperimentDialogData, NewExperimentPaths, Experiment } from '../../../../core/experiments/store/experiments.model';
 import { uuid } from 'uuidv4';
 import { ExperimentFormValidators } from '../../validators/experiment-form.validators';
 
@@ -34,40 +23,25 @@ import { ExperimentFormValidators } from '../../validators/experiment-form.valid
 export class ExperimentDesignComponent implements OnInit {
   @Input() experimentInfo: Experiment;
   @Input() disableControls = false;
-  @Output() emitExperimentDialogEvent = new EventEmitter<
-    NewExperimentDialogData
-  >();
+  @Output() emitExperimentDialogEvent = new EventEmitter<NewExperimentDialogData>();
 
-  @ViewChild('conditionTable', { static: false, read: ElementRef })
-  conditionTable: ElementRef;
-  @ViewChild('segmentTable', { static: false, read: ElementRef })
-  segmentTable: ElementRef;
+  @ViewChild('conditionTable', { static: false, read: ElementRef }) conditionTable: ElementRef;
+  @ViewChild('segmentTable', { static: false, read: ElementRef }) segmentTable: ElementRef;
 
   experimentDesignForm: FormGroup;
   conditionDataSource = new BehaviorSubject<AbstractControl[]>([]);
   segmentDataSource = new BehaviorSubject<AbstractControl[]>([]);
 
-  conditionDisplayedColumns = [
-    'conditionNumber',
-    'conditionCode',
-    'assignmentWeight',
-    'description',
-    'removeCondition'
-  ];
+  conditionDisplayedColumns = [ 'conditionNumber', 'conditionCode', 'assignmentWeight', 'description', 'removeCondition'];
   segmentDisplayedColumns = ['segmentNumber', 'point', 'name', 'removeSegment'];
   constructor(private _formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.experimentDesignForm = this._formBuilder.group(
       {
-        conditions: this._formBuilder.array([
-          this.addConditions(),
-          this.addConditions()
-        ]),
+        conditions: this._formBuilder.array([this.addConditions(), this.addConditions()]),
         segments: this._formBuilder.array([this.addSegments()])
-      },
-      { validators: ExperimentFormValidators.validateExperimentDesignForm }
-    );
+      }, { validators: ExperimentFormValidators.validateExperimentDesignForm });
 
     // populate values in form to update experiment if experiment data is available
     if (this.experimentInfo) {
@@ -76,28 +50,16 @@ export class ExperimentDesignComponent implements OnInit {
       this.condition.removeAt(0);
       this.segment.removeAt(0);
       this.experimentInfo.conditions.forEach(condition => {
-        this.condition.push(
-          this.addConditions(
-            condition.conditionCode,
-            condition.assignmentWeight,
-            condition.description
-          )
-        );
+        this.condition.push(this.addConditions(condition.conditionCode, condition.assignmentWeight, condition.description));
       });
       this.experimentInfo.segments.forEach(segment => {
-        this.segment.push(
-          this.addSegments(segment.point, segment.name, segment.description)
-        );
+        this.segment.push(this.addSegments(segment.point, segment.name, segment.description));
       });
     }
     this.updateView();
   }
 
-  addConditions(
-    conditionCode = null,
-    assignmentWeight = null,
-    description = null
-  ) {
+  addConditions(conditionCode = null, assignmentWeight = null, description = null) {
     return this._formBuilder.group({
       conditionCode: [conditionCode, Validators.required],
       assignmentWeight: [assignmentWeight, Validators.required],
@@ -122,11 +84,9 @@ export class ExperimentDesignComponent implements OnInit {
   }
 
   addConditionOrSegment(type: string) {
-    const form =
-      type === 'condition' ? this.addConditions() : this.addSegments();
+    const form = type === 'condition' ? this.addConditions() : this.addSegments();
     this[type].push(form);
-    const scrollTableType =
-      type === 'condition' ? 'conditionTable' : 'segmentTable';
+    const scrollTableType = type === 'condition' ? 'conditionTable' : 'segmentTable';
     this.updateView(scrollTableType);
   }
 
@@ -156,11 +116,7 @@ export class ExperimentDesignComponent implements OnInit {
           ...this.experimentDesignForm.value
         };
         experimentDesignFormData.conditions = experimentDesignFormData.conditions.map(
-          condition => ({
-            id: uuid(),
-            ...condition,
-            name: ''
-          })
+          condition => ({ id: uuid(), ...condition, name: ''})
         );
         this.emitExperimentDialogEvent.emit({
           type: eventType,
@@ -177,8 +133,7 @@ export class ExperimentDesignComponent implements OnInit {
 
   get isAssignmentWeightControlDirty() {
     if (this.experimentDesignForm) {
-      return (this.experimentDesignForm.controls
-        .conditions as any).controls.some(
+      return (this.experimentDesignForm.controls.conditions as any).controls.some(
         control => control.controls.assignmentWeight.dirty
       );
     }
