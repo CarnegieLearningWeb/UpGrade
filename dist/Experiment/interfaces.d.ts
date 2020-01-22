2e83c5cb424f2ddd4cc34f9b84b95d13de21edfe
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2020-01-22 13:11:47 +0530

    Updating build

--- dist/Experiment/interfaces.d.ts
+++ dist/Experiment/interfaces.d.ts
@@ -2,25 +2,24 @@ export interface IEnrollmentCompleteCondition {
     userCount: number;
     groupCount: number;
 }
+interface IConditionEnrollmentStats {
+    id: string;
+    user: number;
+    group: number;
+}
+interface ISegmentEnrollmentStats {
+    id: string;
+    user: number;
+    group: number;
+    conditions: IConditionEnrollmentStats[];
+}
 export interface IExperimentEnrollmentStats {
     id: string;
     users: number;
     group: number;
     userExcluded: number;
     groupExcluded?: number;
-    conditions: [{
-        conditionId: string;
-        users: number;
-        groups: number;
-    }];
-    segments: [{
-        id: string;
-        user: number;
-        group: number;
-        conditions: [{
-            id: string;
-            user: number;
-            group: number;
-        }];
-    }];
+    conditions: IConditionEnrollmentStats[];
+    segments: ISegmentEnrollmentStats[];
 }
+export {};
