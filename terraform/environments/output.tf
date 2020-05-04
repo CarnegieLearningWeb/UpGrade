39043e0a77a7249d3c21204b3a5ccb72ba0d2f0c
dhavalocked <pdhaval1994.dp@gmail.com>
2020-05-04 16:36:03 +0530

    Fix for one tfstate per env

--- terraform/environments/output.tf
+++ terraform/environments/output.tf
@@ -1,7 +1,7 @@
 output "website-s3-endpoint" {
-  value = "${module.production.*.s3-bucket}"
+  value = "${module.production.s3-bucket}"
 }
 
 output "website-cdn-endpoint" {
-  value = "${module.production.*.s3-cdn}"
+  value = "${module.production.s3-cdn}"
 }
--- terraform/modules/website/codepipeline.tf
+++ terraform/modules/website/codepipeline.tf
@@ -1,12 +1,11 @@
 resource "aws_codepipeline" "prod_pipeline" {
-  count = length(var.environment)
-  name     = "${var.environment[count.index]}-${var.prefix}-pipeline"
-  role_arn = aws_iam_role.codepipeline_role[count.index].arn
+  name     = "${var.environment}-${var.prefix}-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
 
   depends_on = [aws_s3_bucket.bucket_site, aws_s3_bucket.source]
   
   artifact_store {
-    location = aws_s3_bucket.source[count.index].bucket
+    location = aws_s3_bucket.source.bucket
     type     = "S3"
   }
 
@@ -19,11 +18,11 @@ resource "aws_codepipeline" "prod_pipeline" {
       owner            = "AWS"
       provider         = "CodeCommit"
       version          = "1"
-      output_artifacts = ["${var.environment[count.index]}-${var.prefix}-frontend-source"]
+      output_artifacts = ["${var.environment}-${var.prefix}-frontend-source"]
 
       configuration = {
         RepositoryName = aws_codecommit_repository.code_repo.repository_name
-        BranchName     = var.repository_branch[count.index]
+        BranchName     = var.repository_branch
       }
     }
   }
@@ -37,10 +36,10 @@ resource "aws_codepipeline" "prod_pipeline" {
       owner            = "AWS"
       provider         = "CodeBuild"
       version          = "1"
-      input_artifacts  = ["${var.environment[count.index]}-${var.prefix}-frontend-source"]
+      input_artifacts  = ["${var.environment}-${var.prefix}-frontend-source"]
 
       configuration = {
-        ProjectName = aws_codebuild_project.prod_app_build[count.index].name
+        ProjectName = aws_codebuild_project.prod_app_build.name
       }
     }
   }
