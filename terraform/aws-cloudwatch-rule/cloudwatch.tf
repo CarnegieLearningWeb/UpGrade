b45ee1a6c314b612567e972595348bd7271c4481
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2020-05-06 19:18:02 +0530

    Deleting cloudwatch terraform script

--- terraform/aws-cloudwatch-rule/cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_cloudwatch_event_rule" "every_twelve_hour" {
-    name = "every_twelve_hour"
-    description = "Fires every twelve hours"
-    schedule_expression = "rate(5 minutes)"
-}
--- terraform/aws-cloudwatch-rule/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "rule-name" {
-  value = "${aws_cloudwatch_event_rule.every_twelve_hour.name}"
-}
-
