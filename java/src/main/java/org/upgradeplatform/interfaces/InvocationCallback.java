d56cd8d90221027b741eedd33c3083d4d69a63bb
Matt McHenry <mmchenry@carnegielearning.com>
2020-05-07 16:35:17 -0400

    remove unused class
    it appears that ResponseCallback is used everywhere, never InvocationCallback

--- java/src/main/java/org/upgradeplatform/interfaces/InvocationCallback.java
+++
@@ -1,6 +0,0 @@
-package org.upgradeplatform.interfaces;
-
-public interface InvocationCallback<RESPONSE> {
-    public void completed(RESPONSE response);
-    public void failed(Throwable throwable);
-}
