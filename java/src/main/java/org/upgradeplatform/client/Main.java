75a311946636e04665fb4d2cdabab48310209e47
Matt McHenry <mmchenry@carnegielearning.com>
2020-05-12 08:32:19 -0400

    fix Main to await completion of callbacks; improve logging

--- java/src/main/java/org/upgradeplatform/client/Main.java
+++ java/src/main/java/org/upgradeplatform/client/Main.java
@@ -4,6 +4,8 @@ package org.upgradeplatform.client;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.eclipse.jdt.annotation.NonNull;
 import org.upgradeplatform.interfaces.ResponseCallback;
@@ -14,7 +16,7 @@ import org.upgradeplatform.responsebeans.InitRequest;
 
 
 public class Main {
-	public static void main(String []args)
+	public static void main(String []args) throws InterruptedException, ExecutionException
 	{
 		String baseUrl = "http://development-upgrade-experiment-app.eba-gp6psjut.us-east-1.elasticbeanstalk.com";
 		String userId = "user1";
@@ -26,8 +28,11 @@ public class Main {
 		HashMap<String, List<String>> group = new HashMap<>();
 		group.put("classes", Arrays.asList(classList));
 		group.put("teachers", Arrays.asList(teacherList));
-		
+
 		try(ExperimentClient experimentClient = new ExperimentClient(userId, "BearerToken", baseUrl)){
+		    CompletableFuture<String> result = new CompletableFuture<>();
+
+            System.out.println(prefix() + "initiating requests");
 		    experimentClient.setGroupMembership(group, new ResponseCallback<InitRequest>(){
 		        @Override
 		        public void onSuccess(InitRequest __){
@@ -35,25 +40,29 @@ public class Main {
 
 		                @Override
 		                public void onSuccess(@NonNull List<ExperimentsResponse> t) {
-		                    System.out.println(t.size());
-
-		                    //to Close jax-rs client
-		                    experimentClient.close();
+		                    result.complete(prefix() + "retrieved " + t.size() + " experiment responses");
 		                }
 
 		                @Override
 		                public void onError(@NonNull ErrorResponse error) {
-		                    // TODO Auto-generated method stub
-
+                            result.completeExceptionally(new Exception(prefix() + error.toString()));
 		                }
 		            });
 		        }
 
 		        @Override
 		        public void onError(@NonNull ErrorResponse error){
-		            System.err.println(error);
+                    result.completeExceptionally(new Exception(prefix() + error.toString()));
 		        }
 		    });
+
+            System.out.println(prefix() + result.getNow("not complete yet"));
+            String rs = result.get();
+            System.out.println(prefix() + rs);
 		}
+	}
+
+	private static final String prefix() {
+	    return "on thread " + Thread.currentThread().getName() + " at " + System.currentTimeMillis() + ": ";
 	}
 }
