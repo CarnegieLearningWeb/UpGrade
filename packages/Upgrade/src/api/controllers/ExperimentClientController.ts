6a77d2a6db7c0352671af1212c7358df091b3098
Jaydip Hirapara <codecracker2455@gmail.com>
2020-05-12 13:13:30 +0530

    Adding getAllFeatureFlags for SDK

--- packages/Upgrade/src/api/controllers/ExperimentClientController.ts
+++ packages/Upgrade/src/api/controllers/ExperimentClientController.ts
@@ -1,4 +1,4 @@
-import { JsonController, Post, Body } from 'routing-controllers';
+import { JsonController, Post, Body, Get } from 'routing-controllers';
 import { ExperimentService } from '../services/ExperimentService';
 import { ExperimentAssignmentService } from '../services/ExperimentAssignmentService';
 import { MarkExperimentValidator } from './validators/MarkExperimentValidator';
@@ -10,6 +10,8 @@ import { MonitoredExperimentPoint } from '../models/MonitoredExperimentPoint';
 import { IExperimentAssignment } from 'upgrade_types';
 import { FailedParamsValidator } from './validators/FailedParamsValidator';
 import { ExperimentError } from '../models/ExperimentError';
+import { FeatureFlag } from '../models/FeatureFlag';
+import { FeatureFlagService } from '../services/FeatureFlagService';
 
 /**
  * @swagger
@@ -23,7 +25,8 @@ export class ExperimentClientController {
   constructor(
     public experimentService: ExperimentService,
     public experimentAssignmentService: ExperimentAssignmentService,
-    public experimentUserService: ExperimentUserService
+    public experimentUserService: ExperimentUserService,
+    public featureFlagService: FeatureFlagService
   ) {}
 
   /**
@@ -285,5 +288,23 @@ export class ExperimentClientController {
       errorBody.userId,
       errorBody.experimentId
     );
+  }
+
+  /**
+   * @swagger
+   * /featureflags:
+   *    get:
+   *       description: Get all feature flags using SDK
+   *       produces:
+   *         - application/json
+   *       tags:
+   *         - Experiment Point
+   *       responses:
+   *          '200':
+   *            description: Feature flags list
+   */
+  @Get('featureflag')
+  public getAllFlags(): Promise<FeatureFlag[]> {
+    return this.featureFlagService.find();
   }
 }
