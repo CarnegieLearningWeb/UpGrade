97b5b85e9c0cb549cbe8862b809329668a2e33cc
Jaydip Hirapara <codecracker2455@gmail.com>
2020-05-15 15:11:28 +0530

    [App] Remove hammer js

--- projects/abtesting/src/polyfills.ts
+++ projects/abtesting/src/polyfills.ts
@@ -60,5 +60,3 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
-import 'hammerjs';
