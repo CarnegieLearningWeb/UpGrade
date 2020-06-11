c4fbdf3e5190c63a91b9da3d702a6b073afc2c55
Jaydip Hirapara <codecracker2455@gmail.com>
2020-06-11 09:19:02 +0530

    Add query CRUD with experiment update

--- packages/Upgrade/src/api/repositories/QueryRepository.ts
+++ packages/Upgrade/src/api/repositories/QueryRepository.ts
@@ -1,5 +1,48 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, EntityManager } from 'typeorm';
 import { Query } from '../models/Query';
+import repositoryError from './utils/repositoryError';
 
 @EntityRepository(Query)
-export class QueryRepository extends Repository<Query> {}
+export class QueryRepository extends Repository<Query> {
+  public async deleteQuery(id: string, entityManager: EntityManager): Promise<void> {
+    entityManager
+      .createQueryBuilder()
+      .delete()
+      .from(Query)
+      .where('id=:id', { id })
+      .execute()
+      .catch((errorMsg: any) => {
+        const errorMsgString = repositoryError('QueryRepository', 'deleteQuery', { id }, errorMsg);
+        throw new Error(errorMsgString);
+      });
+  }
+
+  public async upsertQuery(
+    queryDoc: Partial<Query>,
+    entityManager: EntityManager
+  ): Promise<Query> {
+    const result = await entityManager
+      .createQueryBuilder()
+      .insert()
+      .into(Query)
+      .values(queryDoc)
+      .onConflict(
+        `("id") DO UPDATE SET "query" = :query, "name" = :name`
+      )
+      .setParameter('query', queryDoc.query)
+      .setParameter('name', queryDoc.name)
+      .returning('*')
+      .execute()
+      .catch((errorMsg: any) => {
+        const errorMsgString = repositoryError(
+          'QueryRepository',
+          'upsertQuery',
+          { queryDoc },
+          errorMsg
+        );
+        throw new Error(errorMsgString);
+      });
+
+    return result.raw[0];
+  }
+}
