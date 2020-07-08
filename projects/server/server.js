f3c2579988a582fb39674f66fcc7e531f6e503b3
Jaydip Hirapara <codecracker2455@gmail.com>
2020-07-08 10:26:24 +0530

    [Fix] Update style and port for express server

--- projects/server/server.js
+++ projects/server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const compression = require('compression');
 
 const CONTEXT = `/${process.env.CONTEXT || ''}`;
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4200;
 
 const app = express();
 
