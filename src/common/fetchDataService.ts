98de773d7fac34538b071e428be46312ca2e6b42
dhrushit <dhrushit@playpowerlabs.com>
2020-07-10 13:10:55 +0530

    setting the keepalive flag when sending post as analytics

--- src/common/fetchDataService.ts
+++ src/common/fetchDataService.ts
@@ -2,13 +2,13 @@ import { Interfaces, Types } from '../identifiers';
 import * as fetch from 'isomorphic-fetch';
 
 // Call this function with url and data which is used in body of request
-export default async function fetchDataService(url: string, token: string, data: any, requestType: Types.REQUEST_TYPES): Promise<Interfaces.IResponse> {
+export default async function fetchDataService(url: string, token: string, data: any, requestType: Types.REQUEST_TYPES, sendAsAnalytics = false): Promise<Interfaces.IResponse> {
   const requestCount = 0;
   const requestThreshold = 5;
-  return await fetchDataFromDB(url, token, data, requestType, requestCount, requestThreshold);
+  return await fetchDataFromDB(url, token, data, requestType, requestCount, requestThreshold, sendAsAnalytics);
 }
 
-async function fetchDataFromDB(url: string, token: string, data: any, requestType: Types.REQUEST_TYPES, requestCount: number, requestThreshold: number): Promise<Interfaces.IResponse> {
+async function fetchDataFromDB(url: string, token: string, data: any, requestType: Types.REQUEST_TYPES, requestCount: number, requestThreshold: number, sendAsAnalytics = false): Promise<Interfaces.IResponse> {
   try {
     let options: any = {
       method: requestType,
@@ -31,10 +31,17 @@ async function fetchDataFromDB(url: string, token: string, data: any, requestTyp
         body: JSON.stringify(data)
       }
     }
+
+    if (sendAsAnalytics === true) {
+      options = {
+        ...options,
+        keepalive: true
+      }
+    }
     const response = await fetch(url, options);
     const responseData = await response.json();
     // If name, endpoint and message appears in response then its error
-    const status =  !responseData.name && !responseData.endPoint && !responseData.message;
+    const status = !responseData.name && !responseData.endPoint && !responseData.message;
     return {
       status,
       data: responseData
