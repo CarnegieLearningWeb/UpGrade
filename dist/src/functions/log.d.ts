e87eae1022a803d96fc65a8f6ac54afe90e03bba
dhrushit <dhrushit@playpowerlabs.com>
2020-07-10 12:31:20 +0530

    adding description

--- dist/src/functions/log.d.ts
+++ dist/src/functions/log.d.ts
@@ -1,4 +1,11 @@
 import { Interfaces } from '../identifiers';
 import { ILogInput } from 'upgrade_types';
 export default function log(url: string, userId: string, token: string, value: ILogInput[]): Promise<Interfaces.ILog[]>;
-export function sendAnalytics(url: string, userId: string, token: string, value: ILogInput[]): void;
+/**
+ *
+ * @param url URL of the host to send the analytics to
+ * @param userId id of the user
+ * @param token additional token to add to the header
+ * @param value data to be sent to the server
+ */
+export declare function sendAnalytics(url: string, userId: string, token: string, value: ILogInput[]): void;
