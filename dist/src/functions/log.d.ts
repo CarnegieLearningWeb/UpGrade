c3a558054a7acef8e3929913df858c8649dc19a7
dhrushit <dhrushit@playpowerlabs.com>
2020-07-10 13:07:59 +0530

    fixing the variable name

--- dist/src/functions/log.d.ts
+++ dist/src/functions/log.d.ts
@@ -1,3 +1,3 @@
 import { Interfaces } from '../identifiers';
 import { ILogInput } from 'upgrade_types';
-export default function log(url: string, userId: string, token: string, value: ILogInput[], sendAnalytics?: boolean): Promise<Interfaces.ILog[]>;
+export default function log(url: string, userId: string, token: string, value: ILogInput[], sendAsAnalytics?: boolean): Promise<Interfaces.ILog[]>;
