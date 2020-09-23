783bd88861d509c4d16dc36ab94b87b68213b40f
Tanmay Jain <tanmay.jain@playpowerlabs.com>
2020-09-23 14:08:10 +0530

    Add revert and TO DO comment

--- src/Experiment/enums.ts
+++ src/Experiment/enums.ts
@@ -11,6 +11,8 @@ export enum ASSIGNMENT_UNIT {
 
 export enum POST_EXPERIMENT_RULE {
   CONTINUE = 'continue',
+  // TO DO : Remove revert when frontend anad backend integrated with assign
+  REVERT = 'revert',
   ASSIGN = 'assign',
 }
 
