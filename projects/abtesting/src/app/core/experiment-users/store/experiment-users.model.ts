63fdf0f001e1816235d0d718e205be012a8d1985
Tanmay Jain <tanmay.jain@playpowerlabs.com>
2020-09-24 10:13:11 +0530

    Update PARTICIPANT to Participant

--- projects/abtesting/src/app/core/experiment-users/store/experiment-users.model.ts
+++ projects/abtesting/src/app/core/experiment-users/store/experiment-users.model.ts
@@ -3,7 +3,7 @@ import { EntityState } from '@ngrx/entity';
 
 export enum EntityTypes {
   GROUP_ID = 'Group ID',
-  PARTICIPANT_ID = 'PARTICIPANT ID'
+  PARTICIPANT_ID = 'Participant ID'
 }
 
 export interface ExcludeEntity {
