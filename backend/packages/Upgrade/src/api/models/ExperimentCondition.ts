9034d9fc6d8bfbce4e421cc9a0f56204f6b47bf1
ppratikcr7 <ppratik.cr7@gmail.com>
2021-05-04 23:22:16 +0530

    Revert "fix for issue 23 backend schema update for assignmentWeight"
    This reverts commit 6c822f067faf917285f1aba54ad7a8e2bb455a42.

--- backend/packages/Upgrade/src/api/models/ExperimentCondition.ts
+++ backend/packages/Upgrade/src/api/models/ExperimentCondition.ts
@@ -28,7 +28,7 @@ export class ExperimentCondition extends BaseModel {
 
   @IsNotEmpty()
   @IsNumber()
-  @Column({ type: "decimal", precision: 5, scale: 2})
+  @Column()
   public assignmentWeight: number;
 
   @ManyToOne((type) => Experiment, (experiment) => experiment.conditions, { onDelete: 'CASCADE' })
