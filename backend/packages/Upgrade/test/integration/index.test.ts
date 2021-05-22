eaa8e97c301abfbbd20beca5f58fbaa953997461
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2021-05-22 16:29:08 +0530

    Fixing mark experiment point when experiment point and Id doesn't exist

--- backend/packages/Upgrade/test/integration/index.test.ts
+++ backend/packages/Upgrade/test/integration/index.test.ts
@@ -34,6 +34,7 @@ import {
 import { MainAuditLog } from './Experiment/auditLogs';
 import { NoPartitionPoint } from './Experiment/onlyExperimentPoint';
 // import { StatsGroupExperiment } from './ExperimentStats';
+import { NoExperiment } from './Experiment/markExperimentPoint';
 import {
   NoPreviewUser,
   PreviewAssignments,
@@ -94,6 +95,11 @@ describe('Integration Tests', () => {
   // -------------------------------------------------------------------------
   // Test cases
   // -------------------------------------------------------------------------
+
+  test('Mark Experiment before experiment is created', async (done) => {
+    await NoExperiment();
+    done();
+  });
 
   test('No Group for Experiment', async (done) => {
     await NoGroup();
