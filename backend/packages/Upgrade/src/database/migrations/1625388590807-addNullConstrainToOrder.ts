2f6d659d56267eb2708c8de7aff602444d069480
Ridham <ridham.s@ahduni.edu.in>
2021-07-05 15:42:59 +0530

    solved lint errors in migration files

--- backend/packages/Upgrade/src/database/migrations/1625388590807-addNullConstrainToOrder.ts
+++ backend/packages/Upgrade/src/database/migrations/1625388590807-addNullConstrainToOrder.ts
@@ -1,7 +1,8 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner} from 'typeorm';
 
+// tslint:disable-next-line: class-name
 export class addNullConstrainToOrder1625388590807 implements MigrationInterface {
-    name = 'addNullConstrainToOrder1625388590807'
+    public name = 'addNullConstrainToOrder1625388590807';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "experiment_condition" ALTER COLUMN "order" DROP NOT NULL`);
