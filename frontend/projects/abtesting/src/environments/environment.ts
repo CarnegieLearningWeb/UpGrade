ea7ab4ebef88148588a1c6268fac01c1a8b14cef
pratik <ppratik.cr7@gmail.com>
2021-08-05 17:38:20 +0530

    updating new cname for bsnl and dev backend

--- frontend/projects/abtesting/src/environments/environment.ts
+++ frontend/projects/abtesting/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-const endpointApi = 'https://development-upgrade-experiment-app.eba-gp6psjut.us-east-1.elasticbeanstalk.com/api';
+// const endpointApi = 'https://development-upgrade-experiment-app.eba-gp6psjut.us-east-1.elasticbeanstalk.com/api';
+const endpointApi = 'https://upgrade-dev-backend.edoptimize.com/api'
 
 export const environment = {
   appName: 'UpGrade',
