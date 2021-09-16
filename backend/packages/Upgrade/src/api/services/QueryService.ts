2fd60d6b17a29455a03640f390ae72f82f5b51bd
YashilDepani <yashildepani@gmail.com>
2021-09-16 11:55:55 +0530

    decoupling metric queries

--- backend/packages/Upgrade/src/api/services/QueryService.ts
+++ backend/packages/Upgrade/src/api/services/QueryService.ts
@@ -4,12 +4,15 @@ import { QueryRepository } from '../repositories/QueryRepository';
 import { Logger, LoggerInterface } from '../../decorators/Logger';
 import { Query } from '../models/Query';
 import { LogRepository } from '../repositories/LogRepository';
+import { SERVER_ERROR } from 'upgrade_types';
+import { ErrorService } from './ErrorService';
 
 @Service()
 export class QueryService {
   constructor(
     @OrmRepository() private queryRepository: QueryRepository,
     @OrmRepository() private logRepository: LogRepository,
+    public errorService: ErrorService,
     @Logger(__filename) private log: LoggerInterface
   ) {}
 
@@ -34,13 +37,35 @@ export class QueryService {
 
     const promiseResult = await Promise.all(promiseArray);
     const analysePromise = promiseResult.map((query) => this.logRepository.analysis(query));
-    let response = await Promise.all(analysePromise);
-    response = response.map((res, index) => {
-      return {
-        id: queryIds[index],
-        result: res,
-      };
+    const response = await Promise.allSettled(analysePromise);
+
+    const failedQuery = Array<Promise<any>>();
+
+    let modifiedResponse = response.map((query, index) => {
+      if (query.status === 'fulfilled') {
+        return query.value;
+      } else {
+        this.log.error('Error in Query Id ', queryIds[index]);
+        failedQuery.push(this.errorService.create({
+          endPoint: '/api/query/analyse',
+          errorCode: 500,
+          message: `Query Failed error: ${JSON.stringify(queryIds[index], undefined, 2)}`,
+          name: 'Query Failed error',
+          type: SERVER_ERROR.QUERY_FAILED,
+        } as any));
+
+        return [];
+      }
     });
-    return response;
+
+    if (failedQuery.length) {
+      await Promise.all(failedQuery);
+    }
+
+    modifiedResponse = modifiedResponse.map((res, index) => {
+      return queryIds[index] ? { id: queryIds[index], result: res} : null;
+    }).filter(query => query);
+
+    return modifiedResponse;
   }
 }
