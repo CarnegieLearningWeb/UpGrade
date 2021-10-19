194e5f65d54cf78830e018b326cd6a15f9566d76
pratik <ppratik.cr7@gmail.com>
2021-10-19 22:44:40 +0530

    splunk logger format working with child loggers in logmiddleware request

--- backend/packages/Upgrade/src/api/middlewares/LogMiddleware.ts
+++ backend/packages/Upgrade/src/api/middlewares/LogMiddleware.ts
@@ -1,18 +1,53 @@
-import * as express from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import { ExpressMiddlewareInterface, Middleware } from 'routing-controllers';
-
+import uuid from 'uuid';
+import winston, { format, transports } from 'winston';
+import SplunkStreamEvent from 'winston-splunk-httplogger';
 import { env } from '../../env';
-import { Logger } from '../../lib/logger';
+// import { Logger } from '../../lib/logger';
+import { SplunkLogger } from '../../lib/logger/SplunkLogger';
+
+// var httpRequestId = require('express-request-id')();
+// console.log("express request id: ", httpRequestId);
+
+let logger = winston.createLogger({
+  transports: [
+    new transports.Console({
+      level: env.log.level,
+      handleExceptions: true,
+      format:
+        env.node !== 'development'
+          ? format.combine(format.json())
+          : format.combine(format.colorize(), format.simple()),
+    }),
+    new SplunkStreamEvent({ splunk: SplunkLogger.splunkSettings})
+  ]
+});
+declare global {
+  namespace Express {
+    interface Request {
+      logger: any
+    }
+  }
+}
 
 @Middleware({ type: 'before' })
 export class LogMiddleware implements ExpressMiddlewareInterface {
-  private log = new Logger(__dirname);
-
+  // private log = new Logger(__dirname);
+  
   public use(req: express.Request, res: express.Response, next: express.NextFunction): any {
+    //childlogger creation
+    logger = logger.child({ http_request_id: uuid(), endpoint: req.url, client_session_id: uuid(), api_request_type: "Server Side"});
+    logger.info('In log middleware:' + Date.now(), logger);
+    // logger.info('In log middleware:' + Date.now(), SplunkLogger.payload_json);
+    // adding logger instance to the request to pass it to all other requests:
+    req.logger = logger;
+    // console.log("req: ", req);
+
     return morgan(env.log.output, {
       stream: {
-        write: this.log.info.bind(this.log),
+        write: logger.info.bind(logger),
       },
     })(req, res, next);
   }
--- backend/packages/Upgrade/src/app.ts
+++ backend/packages/Upgrade/src/app.ts
@@ -9,8 +9,8 @@ import 'reflect-metadata';
 
 import { bootstrapMicroframework } from 'microframework';
 import { expressLoader } from './loaders/expressLoader';
-import { banner } from './lib/banner';
-import { Logger } from './lib/logger/Logger';
+// import { banner } from './lib/banner';
+// import { Logger } from './lib/logger/Logger';
 import { winstonLoader } from './loaders/winstonLoader';
 import { homeLoader } from './loaders/homeLoader';
 import { publicLoader } from './loaders/publicLoader';
@@ -19,20 +19,39 @@ import { typeormLoader } from './loaders/typeormLoader';
 import { swaggerLoader } from './loaders/swaggerLoader';
 import { CreateSystemUser } from './init/seed/systemUser';
 import { enableMetricFiltering } from './init/seed/EnableMetricFiltering';
+import winston, { format, transports } from 'winston';
+import SplunkStreamEvent from 'winston-splunk-httplogger';
+import { SplunkLogger } from './lib/logger/SplunkLogger';
+
+
+let logger = winston.createLogger({
+  transports: [
+    new transports.Console({
+      level: env.log.level,
+      handleExceptions: true,
+      format:
+        env.node !== 'development'
+          ? format.combine(format.json())
+          : format.combine(format.colorize(), format.simple()),
+    }),
+    new SplunkStreamEvent({ splunk: SplunkLogger.splunkSettings})
+  ]
+});
 
 /*
  * EXPRESS TYPESCRIPT BOILERPLATE
  * ----------------------------------------
  */
 
-const log = new Logger(__filename);
+// const log = new Logger(__filename);
 
 bootstrapMicroframework({
   loaders: [winstonLoader, iocLoader, typeormLoader, expressLoader, swaggerLoader, homeLoader, publicLoader],
 })
   .then(() => {
+    // logger = logger.child({requestId: '1'});
     // logging data after the winston is configured
-    log.info('Server starting at', Date.now());
+    logger.info('Server starting at ' + Date.now());
     return CreateSystemUser();
   })
   .then(() => {
@@ -40,5 +59,5 @@ bootstrapMicroframework({
     return enableMetricFiltering();
   })
   .then(() => {
-    banner(log);
+    // banner(childLogger);
   });
