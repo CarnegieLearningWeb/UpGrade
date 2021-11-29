22e59132bb33380966d7e745a6c8edbe42928946
Zack Lee <zlee@carnegielearning.com>
2021-11-29 10:06:13 -0500

    Update demo-app.js

--- frontend/projects/upgrade/src/assets/js/demo-app.js
+++ frontend/projects/upgrade/src/assets/js/demo-app.js
@@ -1,86 +1,128 @@
-// This file is for making UpGrade communicate with the Demo App
-
-// Execute the code if the page is inside an iframe
-if (window.self !== window.top) {
+// This file is for making UpGrade communicate with the demo app
+if (window.self !== window.top) { // Execute the code if the page is inside an iframe
+    // The demo app's URL
     const demoAppURL = "http://localhost:8080";
 
-    // Receive messages from the parent window (Demo App)
+    // For getting UpGrade's element by ID
+    const getElementById = (id) => {
+        switch (id) {
+            case "login-with-google-button":
+                return document.querySelector("div.login-container button.google-sign-in-btn");
+            case "experiments-tab":
+                return document.querySelectorAll("div.list-item-container a.nav-item")[0];
+            case "feature-flags-tab":
+                return document.querySelectorAll("div.list-item-container a.nav-item")[1];
+            case "participants-tab":
+                return document.querySelectorAll("div.list-item-container a.nav-item")[2];
+            case "logs-tab":
+                return document.querySelectorAll("div.list-item-container a.nav-item")[3];
+            case "profile-tab":
+                return document.querySelector("div.user a.profile");
+            case "signout-button":
+                return document.querySelector("div.mat-list-item-content a.logout-link");
+            case "import-experiment-button":
+                return document.querySelectorAll("mat-card.mat-card button.mat-flat-button")[0];
+            case "import-experiment-choose-file-button":
+                return document.querySelector("div.import-experiment-modal input.file-input");
+            case "import-experiment-close-button":
+                return document.querySelectorAll("div.import-experiment-modal button.mat-raised-button")[0];
+            case "import-experiment-import-button":
+                return document.querySelectorAll("div.import-experiment-modal button.mat-raised-button")[1];
+            case "add-experiment-button":
+                return document.querySelectorAll("mat-card.mat-card button.mat-flat-button")[1];
+            case "add-experiment-close-button":
+                return document.querySelectorAll("div.new-experiment-modal button.mat-raised-button")[0];
+            case "add-experiment-next-button":
+                return document.querySelectorAll("div.new-experiment-modal button.mat-raised-button")[1];
+            case "experiment-name-area-concrete-or-abstract":
+                return Array.from(document.querySelectorAll("div.experiment-list-table-container a.experiment-name")).find(elem => elem.textContent.trim() === "Area - Concrete or Abstract");
+            case "experiment-detail-status":
+                return document.querySelectorAll("div.section-data span.section-data-value--editable")[0];
+            case "experiment-detail-status-new-status-select":
+                return document.querySelectorAll("div.mat-form-field-wrapper div.mat-form-field-flex")[5];
+            case "experiment-detail-status-new-status-select-preview":
+                return document.querySelectorAll("div.mat-select-panel mat-option.mat-option")[0];
+            case "experiment-detail-status-new-status-select-scheduled":
+                return document.querySelectorAll("div.mat-select-panel mat-option.mat-option")[1];
+            case "experiment-detail-status-new-status-select-enrolling":
+                return document.querySelectorAll("div.mat-select-panel mat-option.mat-option")[2];
+            case "experiment-detail-status-new-status-select-cancelled":
+                return document.querySelectorAll("div.mat-select-panel mat-option.mat-option")[3];
+            case "experiment-detail-status-close-button":
+                return document.querySelectorAll("div.experiment-status button.modal-btn")[0];
+            case "experiment-detail-status-save-button":
+                return document.querySelectorAll("div.experiment-status button.modal-btn")[1];
+            case "experiment-detail-post-experiment-rule":
+                return document.querySelectorAll("div.section-data span.section-data-value--editable")[1];
+            case "experiment-detail-enrollment-time-logs":
+                return document.querySelectorAll("div.section-data span.section-data-value--editable")[2];
+            case "experiment-detail-ending-criteria":
+                return document.querySelector("div.end-criteria-container span.end-criteria-underline--grey");
+            case "experiment-detail-edit-button":
+                return document.querySelectorAll("div.action-buttons button.mat-flat-button")[0];
+            case "experiment-detail-monitor-button":
+                return document.querySelectorAll("div.action-buttons button.mat-flat-button")[1];
+            case "experiment-detail-delete-button":
+                return document.querySelectorAll("div.action-buttons button.mat-flat-button")[2];
+            case "experiment-detail-export-button":
+                return document.querySelectorAll("div.action-buttons button.mat-flat-button")[3];
+        }
+        console.error(`Error: The element ID "${id}" is not valid.`);
+        return null;
+    }
+
+    // A set of functions that do more then triggering an event
+    const initCallFunction = {
+        "set-zoom-level": (args) => {
+            document.body.style.zoom = args[0];
+        },
+        "logout": () => {
+            const elem = getElementById("signout-button");
+            if (elem) {
+                elem.click();
+            } else if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                gapi.auth2.getAuthInstance().signOut();
+            }
+        }
+    };
+
+    // Used to call a function or trigger event on element
+    const initCall = (type, id, args) => {
+        if (type === "function") {
+            return initCallFunction[id](args);
+        }
+        const elem = getElementById(id);
+        if (elem) {
+            elem[type](...args);
+        }
+    }
+
+    // Used to detect event on element
+    const nextCallback = (type, id, onNextCall) => {
+        const onWindowEvent = (event) => {
+            const elem = getElementById(id);
+            if (elem && elem.contains(event.target)) {
+                window.removeEventListener(type, onWindowEvent, true);
+                onNextCall();
+            }
+        }
+        window.addEventListener(type, onWindowEvent, true);
+    }
+
+    // Receive data from the demo app
     window.addEventListener("message", (event) => {
         if (event.origin !== demoAppURL) {
             return;
         }
         const data = event.message || event.data;
-        switch (data.message) {
-            case "setZoomLevel":
-                document.body.style.zoom = data.value;
-                break;
-            case "logout":
-                if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
-                    gapi.auth2.getAuthInstance().signOut();
-                }
-                break;
-            case "clickExperimentsTab":
-                const experimentsTab = document.querySelectorAll("div.list-item-container a.nav-item")[0];
-                if (experimentsTab) {
-                    experimentsTab.click();
-                }
-                break;
+        if (data.message === "initCall") {
+            initCall(data.type, data.id, data.args || []);
         }
-    });
-    // Detect click events
-    window.addEventListener("click", (event) => {
-        // Store the list of clickable elements in the current page
-        let elements = [];
-        const pathname = new URL(window.location.href).pathname;
-        if (pathname.startsWith("/home/detail")) {
-        }
-        else if (pathname.startsWith("/home")) {
-            elements = [
-                {
-                    name: "importExperiment",
-                    query: document.querySelectorAll("mat-card button.mat-flat-button")[0]
-                },
-                {
-                    name: "importExperimentChooseFile",
-                    query: document.querySelector("div.import-experiment-modal input.file-input")
-                },
-                {
-                    name: "importExperimentClose",
-                    query: document.querySelectorAll("div.import-experiment-modal button.mat-raised-button")[0]
-                },
-                {
-                    name: "importExperimentImport",
-                    query: document.querySelectorAll("div.import-experiment-modal button.mat-raised-button")[1]
-                },
-                {
-                    name: "addExperiment",
-                    query: document.querySelectorAll("mat-card button.mat-flat-button")[1]
-                },
-                {
-                    name: "addExperimentClose",
-                    query: document.querySelectorAll("div.new-experiment-modal button.mat-raised-button")[0]
-                },
-                {
-                    name: "addExperimentNext",
-                    query: document.querySelectorAll("div.new-experiment-modal button.mat-raised-button")[1]
-                }
-            ];
-        }
-        else if (pathname.startsWith("/login")) {
-            elements = [
-                {
-                    name: "LoginWithGoogle",
-                    query: document.querySelector("div.login-container button.google-sign-in-btn")
-                }
-            ];
-        }
-
-        // Detect if any clickable element is clicked, and send its name to the parent window (Demo App)
-        for (const element of elements) {
-            if (element.query && element.query.contains(event.target)) {
-                window.parent.postMessage({ message: "fromUpgrade", value: element.name }, demoAppURL);
-                break;
-            }
+        else if (data.message === "nextCallback") {
+            nextCallback(data.type, data.id, () => {
+                // Send data back to the demo app
+                window.parent.postMessage({ message: "onNextCall" }, demoAppURL);
+            });
         }
     });
 }
