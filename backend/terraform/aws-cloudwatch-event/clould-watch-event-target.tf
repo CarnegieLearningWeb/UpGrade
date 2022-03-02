4a22ca829dce5ef16ad2000aadf6bbd4a7e76579
Pratik Prajapati <33730817+ppratikcr7@users.noreply.github.com>
2022-03-02 20:27:50 +0530

    Changing mispelled Schedular to Scheduler (#253)
    * Changing mispelled Schedular to Scheduler
    
    * Scheduler spelling correction in new files from dev merge
    
    Co-authored-by: Mike Fugate <mfugate@carnegielearning.com>

--- backend/terraform/aws-cloudwatch-event/clould-watch-event-target.tf
+++ backend/terraform/aws-cloudwatch-event/clould-watch-event-target.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudwatch_event_target" "clear_logs_after_twelve_hours" {
     rule = aws_cloudwatch_event_rule.every_twelve_hour.name
-    target_id = "test-checking-schedular"
+    target_id = "test-checking-scheduler"
     arn = var.lambda_arn
     input = <<DOC
     {
