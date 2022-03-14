93a8ed6ca58d8f27b5cc9840dc294c64bbddf48d
Zack Lee <zlee@carnegielearning.com>
2022-03-14 12:15:49 -0400

    Add services unit testing

--- backend/packages/Upgrade/test/unit/services/AuditService.test.ts
+++ backend/packages/Upgrade/test/unit/services/AuditService.test.ts
@@ -6,8 +6,7 @@ import { ExperimentAuditLogRepository } from '../../../src/api/repositories/Expe
 import { EXPERIMENT_LOG_TYPE } from '../../../../../../types/src';
 
 
-//CHANGE MOCK ARR APPROPRIATELY
-let auditArr = [1, 2, 3]
+let auditArr = [1, 2, 3];
 
 
 describe('Audit Service Testing', () => {
@@ -24,7 +23,8 @@ describe('Audit Service Testing', () => {
                 {
                     provide: getRepositoryToken(ExperimentAuditLogRepository),
                     useValue: {
-                        find: jest.fn().mockResolvedValue(auditArr), //ADD OTHER CALLED FUNCTIONS TO MOCK RETURN VALUES
+                        find: jest.fn().mockResolvedValue(auditArr),
+                        paginatedFind: jest.fn().mockResolvedValue(auditArr),
                         getTotalLogs: jest.fn().mockResolvedValue(auditArr.length)
                     }
                 }
@@ -32,7 +32,7 @@ describe('Audit Service Testing', () => {
         }).compile()
 
         service = module.get<AuditService>(AuditService);
-        repo = module.get<Repository<ExperimentAuditLogRepository>>(getRepositoryToken(ExperimentAuditLogRepository))
+        repo = module.get<Repository<ExperimentAuditLogRepository>>(getRepositoryToken(ExperimentAuditLogRepository));
     })
 
 
@@ -44,15 +44,18 @@ describe('Audit Service Testing', () => {
         expect(await repo.find()).toEqual(auditArr)
     })
 
-    it('should return an array of audit logs by type', async() => {
-        const flags = await service.getAuditLogByType(EXPERIMENT_LOG_TYPE.EXPERIMENT_CREATED);
-        //CHANGE THE MOCK VALUE APPROPRIATELY
+    it('should return a count of audit logs', async() => {
+        const flags = await service.getTotalLogs(EXPERIMENT_LOG_TYPE.EXPERIMENT_CREATED);
+        expect(flags).toEqual(auditArr.length)
+    })
+
+    it('should return an array of audit logs', async() => {
+        const flags = await service.getAuditLogs(1, 0);
         expect(flags).toEqual(auditArr)
     })
 
-    it('should return a count of audit logs', async() => {
-        const flags = await service.getTotalLogs(EXPERIMENT_LOG_TYPE.EXPERIMENT_CREATED);
-        //CHANGE THE MOCK VALUE APPROPRIATELY
-        expect(flags).toEqual(auditArr.length)
+    it('should return an array of audit logs by type', async() => {
+        const flags = await service.getAuditLogByType(EXPERIMENT_LOG_TYPE.EXPERIMENT_CREATED);
+        expect(flags).toEqual(auditArr)
     })
 });
