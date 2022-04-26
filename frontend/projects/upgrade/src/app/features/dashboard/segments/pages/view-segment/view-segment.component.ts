6836a163bc074ddae6dbd425e518175d98ae1194
RidhamShah <ridham.shah@ahduni.edu.in>
2022-04-26 19:52:12 +0530

    Created segment view component

--- frontend/projects/upgrade/src/app/features/dashboard/segments/pages/view-segment/view-segment.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/segments/pages/view-segment/view-segment.component.ts
@@ -4,8 +4,6 @@ import { Subscription } from 'rxjs';
 import { MatDialog } from '@angular/material';
 import { AuthService } from '../../../../../core/auth/auth.service';
 import { filter } from 'rxjs/operators';
-import { FeatureFlag } from '../../../../../core/feature-flags/store/feature-flags.model';
-import { FeatureFlagsService } from '../../../../../core/feature-flags/feature-flags.service';
 import { SegmentsService } from '../../../../../core/segments/segments.service';
 import { NewSegmentComponent } from '../../components/modal/new-flag/new-segment.component';
 import * as clonedeep from 'lodash.clonedeep';
@@ -20,15 +18,13 @@ import { Segment } from '../../../../../core/segments/store/segments.model';
 export class ViewSegmentComponent implements OnInit, OnDestroy {
   permissions: UserPermission;
   permissionsSub: Subscription;
-  flag: FeatureFlag;
   segment: any;
-  flagSub: Subscription;
   segmentSub: Subscription;
+  members: {type: string, id: string}[] = [];
   
-  displayedVariationColumns: string[] = ['variationNumber', 'value', 'name', 'description'];
+  displayedVariationColumns: string[] = ['variationNumber', 'value', 'name'];
 
   constructor(
-    private featureFlagsService: FeatureFlagsService,
     private segmentsService: SegmentsService,
     private dialog: MatDialog,
     private authService: AuthService) { }
@@ -37,54 +33,58 @@ export class ViewSegmentComponent implements OnInit, OnDestroy {
     this.permissionsSub = this.authService.userPermissions$.subscribe(permission => {
       this.permissions = permission;
     });
-    this.flagSub = this.featureFlagsService.selectedFeatureFlag$
-      .pipe(filter(flag => !!flag))
-      .subscribe(flag => {
-        this.flag = flag;
-      });
 
-      this.segmentSub = this.segmentsService.selectedSegment$
+    this.segmentSub = this.segmentsService.selectedSegment$
       .pipe(filter(segment => !!segment))
       .subscribe(segment => {
         this.segment = segment;
+    });
+
+    if (this.segment) {
+      this.segment.individualForSegment.forEach(user => {
+        this.members.push({type: 'Individual', id: user.userId})
       });
 
-      if(this.segment)
-      {
-        console.log(' the backend recerived data is -----------------', this.segment);
-      }
+      this.segment.groupForSegment.forEach(group => {
+        this.members.push({type: group.type, id: group.groupId})
+      });
+
+      this.segment.subSegments.forEach(subSegment => {
+        this.members.push({type: 'Segment', id: subSegment.name})
+      });
+    }
   }
 
-  openEditFlagDialog() {
+  openEditSegmentDialog() {
     const dialogRef = this.dialog.open(NewSegmentComponent as any, {
       panelClass: 'new-segment-modal',
-      data: { flagInfo: clonedeep(this.flag) }
+      data: { segmentInfo: clonedeep(this.segment) }
     });
 
     dialogRef.afterClosed().subscribe(() => { });
   }
 
-  deleteFlag() {
+  deleteSegment() {
     const dialogRef = this.dialog.open(DeleteSegmentComponent, {
       panelClass: 'delete-modal',
-      data: { flagName: this.flag.name, flagId: this.flag.id }
+      data: { segmentName: this.segment.name, segmentId: this.segment.id }
     });
 
     dialogRef.afterClosed().subscribe(() => {
-      // Add code of further actions after deleting feature flag
+      // Add code of further actions after deleting feature segment
     });
   }
 
-  getActiveVariation(flag: FeatureFlag) {
-    return this.featureFlagsService.getActiveVariation(flag);
+  duplicateSegmentDialog() {
   }
 
-  changeFlagStatus(flagId: string, event: any) {
-    this.featureFlagsService.updateFeatureFlagStatus(flagId, event.checked);
+  exportSegment(segmentId: string) {
+    // this.experimentService.exportExperimentDesign(experimentId);
+    // this.openSnackBar(false);
   }
 
   ngOnDestroy() {
-    this.flagSub.unsubscribe();
+    this.segmentSub.unsubscribe();
     this.permissionsSub.unsubscribe();
   }
 }
