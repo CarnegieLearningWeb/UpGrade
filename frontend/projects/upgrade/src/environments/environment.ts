dba925d2d732c956e25d586827b1a95f54ad0a36
pratik <ppratik.cr7@gmail.com>
2022-05-10 14:25:05 +0530

    localhost backend url missing

--- frontend/projects/upgrade/src/environments/environment.ts
+++ frontend/projects/upgrade/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-const endpointApi = 'http://development-upgrade-experiment-app.eba-gp6psjut.us-east-1.elasticbeanstalk.com/api';
+const endpointApi = 'http://localhost:3030/api';
+//const endpointApi = 'http://development-upgrade-experiment-app.eba-gp6psjut.us-east-1.elasticbeanstalk.com/api';
 
 export const environment = {
   appName: 'UpGrade',
