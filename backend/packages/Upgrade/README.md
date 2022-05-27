f11ea16ed15603a0f925e8fc836814c222781840
Jennifer Reddig <jreddig@carnegielearning.com>
2022-05-27 10:20:01 -0400

    unit tests for include exclude

--- backend/packages/Upgrade/README.md
+++ backend/packages/Upgrade/README.md
@@ -1,6 +1,6 @@
 Backend Unit Test Coverage
 
-![](https://img.shields.io/badge/Coverage-42%25-F2C572.svg?style=flat&logo=jest&label=Statements&prefix=$statements$) ![](https://img.shields.io/badge/Coverage-16%25-F2C572.svg?style=flat&logo=jest&label=Branches&prefix=$branches$) ![](https://img.shields.io/badge/Coverage-21%25-F2C572.svg?style=flat&logo=jest&label=Functions&prefix=$functions$) ![](https://img.shields.io/badge/Coverage-40%25-F2C572.svg?style=flat&logo=jest&label=Lines&prefix=$lines$)
+![](https://img.shields.io/badge/Coverage-42%25-F2C572.svg?style=flat&logo=jest&label=Statements&prefix=$statements$) ![](https://img.shields.io/badge/Coverage-17%25-F2C572.svg?style=flat&logo=jest&label=Branches&prefix=$branches$) ![](https://img.shields.io/badge/Coverage-23%25-F2C572.svg?style=flat&logo=jest&label=Functions&prefix=$functions$) ![](https://img.shields.io/badge/Coverage-41%25-F2C572.svg?style=flat&logo=jest&label=Lines&prefix=$lines$)
 
 Overall (average of all 4) Goal: 80%
 
---
+++ backend/packages/Upgrade/test/unit/controllers/ExcludeController.test.ts
@@ -0,0 +1,86 @@
+import app from '../../utils/expressApp';
+import request from 'supertest';
+import { configureLogger } from '../../utils/logger';
+import { useContainer as routingUseContainer } from 'routing-controllers';
+import { Container } from 'typedi';
+import uuid from 'uuid/v4';
+import ExcludeServiceMock from './mocks/ExcludeServiceMock';
+import { ExcludeService } from '../../../src/api/services/ExcludeService';
+
+import { useContainer as classValidatorUseContainer } from 'class-validator';
+import { useContainer as ormUseContainer } from 'typeorm';
+
+describe('Exclude Controller Testing', () => {
+  beforeAll(() => {
+    configureLogger();
+    routingUseContainer(Container);
+    ormUseContainer(Container);
+    classValidatorUseContainer(Container);
+
+    // set mock container
+    Container.set(ExcludeService, new ExcludeServiceMock());
+  });
+
+  afterAll(() => {
+    Container.reset();
+  });
+
+  const excludedGroups = {
+    "type": "schoolId",
+    "id": "abc123"
+  }
+
+  const user_uuid = uuid();
+
+  test('Get request for /api/explicitExclude/global/user', async done => {
+    await request(app)
+      .get('/api/explicitExclude/global/user')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+
+  test('Put request for /api/explicitExclude/global/user', async done => {
+    await request(app)
+      .put('/api/explicitExclude/global/user')
+      .send(user_uuid)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+
+  test('Delete request for /api/explicitExclude/global/user/:id', async done => {
+    await request(app)
+      .delete(`/api/explicitExclude/global/user/${user_uuid}`)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+
+  test('Get request for /api/explicitExclude/global/group', async done => {
+    await request(app)
+      .get('/api/explicitExclude/global/group')
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+
+  test('Put request for /api/explicitExclude/global/group', async done => {
+    await request(app)
+      .put('/api/explicitExclude/global/group')
+      .send(excludedGroups)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+
+  test('Delete request for /api/explicitExclude/global/group/:type/:id', async done => {
+    await request(app)
+      .delete(`/api/explicitExclude/global/group/${excludedGroups.type}/${excludedGroups.id}`)
+      .send(excludedGroups)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    done();
+  });
+  
+});
