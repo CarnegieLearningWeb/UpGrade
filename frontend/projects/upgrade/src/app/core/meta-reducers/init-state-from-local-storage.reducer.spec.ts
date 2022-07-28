a8e9ebbf2f47554003ea63aae91af25446fc67ea
danoswaltCL <97542869+danoswaltCL@users.noreply.github.com>
2022-07-28 11:23:06 -0400

    meta-reducer specs (#469)

---
+++ frontend/projects/upgrade/src/app/core/meta-reducers/init-state-from-local-storage.reducer.spec.ts
@@ -0,0 +1,17 @@
+import { createAction, createReducer } from '@ngrx/store';
+import { AppState } from '../core.module';
+import { initStateFromLocalStorage } from './init-state-from-local-storage.reducer';
+
+describe("initStateFromLocalStorage", () => {
+    it('should just return state if INIT or UPDATE is not the action called', () => {
+        const previousState: AppState = {
+            router: null
+        };
+        const reducer = createReducer(previousState);
+    
+        const metaReducer = initStateFromLocalStorage(reducer);
+        const newState = metaReducer(previousState, createAction('[Logs] Get Audit Logs Success'))
+
+        expect(newState).toEqual(previousState);
+    })
+})
