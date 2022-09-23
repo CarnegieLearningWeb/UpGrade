a9303fa16a3066e9fc9850bbac9a6b87aa2ed0d3
Dan Oswalt <doswalt@carnegielearning.com>
2022-09-23 12:01:40 -0400

    initial setup w/ prettier and eslint

---
+++ regression/src/index.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+// console.log("hi");
+
+// const getBreeds = async () => {
+//   try {
+//     return await axios.get("https://dog.ceo/api/breeds/list/all");
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// const result = await getBreeds();
+
+// console.log(result?.data);
