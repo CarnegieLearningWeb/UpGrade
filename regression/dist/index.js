1c2435f6f02937f096895f30c3a4341e49028987
Dan Oswalt <doswalt@carnegielearning.com>
2022-09-28 17:05:45 -0400

    proper gitignore

--- regression/dist/index.js
+++
@@ -1,4 +0,0 @@
-import { ExcludeIfReachedTests } from "./ExcludeIfReachedTests";
-import { env } from "./env";
-const excludeIfReachedSuite = new ExcludeIfReachedTests(env.local);
-excludeIfReachedSuite.run();
