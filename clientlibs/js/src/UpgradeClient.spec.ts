2e31740ef314f06e527f85a6421b05cc53c1fb51
Dan Oswalt <doswalt@carnegielearning.com>
2022-11-02 10:39:53 -0400

    adding test spec

--- clientlibs/js/src/UpgradeClient.spec.ts
+++ clientlibs/js/src/UpgradeClient.spec.ts
@@ -1,9 +1,42 @@
 import UpgradeClient from './UpgradeClient';
 
-describe('UpgradeClient', () => {
-  it('should create client', () => {
-    const client = new UpgradeClient('testUser123', 'http://localhost:3030');
+describe('UpgradeClient quick local tests', () => {
+  let client: UpgradeClient;
+  const hostUrl = 'http://localhost:3030';
+  const token = 'someToken';
 
+  it('should create client', () => {
+    const userId = 'testUserCreate';
+    client = new UpgradeClient(userId, hostUrl, { token });
     expect(client).toBeTruthy();
+  });
+
+  describe('init', () => {
+    it('should successfully init user with no group or workingGroup', async () => {
+      const userId = 'testUserInit1';
+      const expectedResponse = { id: userId, group: null, workingGroup: null };
+
+      client = new UpgradeClient(userId, hostUrl, { token });
+      const response = await client.init();
+
+      expect(response).toEqual(expectedResponse);
+    });
+
+    it('should successfully init user with valid group', async () => {
+      const userId = 'testUserInit4';
+      const group = new Map();
+      group.set('schoolId', ['washington']);
+      group.set('classId', ['qwerty', 'yuiop']);
+      console.log(group);
+
+      client = new UpgradeClient(userId, hostUrl, { token });
+      const expectedResponse = { id: userId, group, workingGroup: null };
+
+      const response = await client.init(group);
+
+      console.log(response);
+
+      expect(response).toEqual(expectedResponse);
+    });
   });
 });
