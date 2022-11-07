22211942bebcb885779fc5316b783b3ca3de53aa
Dan Oswalt <doswalt@carnegielearning.com>
2022-11-07 10:40:59 -0500

    add eslint and modify npm scripts

--- backend/packages/Upgrade/src/lib/banner.ts
+++ backend/packages/Upgrade/src/lib/banner.ts
@@ -4,18 +4,18 @@ import { UpgradeLogger } from './logger/UpgradeLogger';
 export function banner(log: UpgradeLogger): void {
   if (env.app.banner) {
     const route = () => `${env.app.schema}://${env.app.host}:${env.app.port}`;
-    log.info({ message: `Aloha, your app is ready on ${route()}${env.app.routePrefix}`});
-    log.info({ message: `To shut it down, press <CTRL> + C at any time.`});
-    log.info({ message: `Environment  : ${env.node}`});
-    log.info({ message: `Version      : ${env.app.version}`});
-    log.info({ message: `API Info     : ${route()}${env.app.routePrefix}`});
+    log.info({ message: `Aloha, your app is ready on ${route()}${env.app.routePrefix}` });
+    log.info({ message: `To shut it down, press <CTRL> + C at any time.` });
+    log.info({ message: `Environment  : ${env.node}` });
+    log.info({ message: `Version      : ${env.app.version}` });
+    log.info({ message: `API Info     : ${route()}${env.app.routePrefix}` });
     if (env.swagger.enabled) {
-      log.info({ message: `Swagger      : ${route()}${env.swagger.route}`});
+      log.info({ message: `Swagger      : ${route()}${env.swagger.route}` });
     }
     // if (env.monitor.enabled) {
     //   log.info(`Monitor      : ${route()}${env.monitor.route}`);
     // }
   } else {
-    log.info({ message: `Application is up and running.`});
+    log.info({ message: `Application is up and running.` });
   }
 }
