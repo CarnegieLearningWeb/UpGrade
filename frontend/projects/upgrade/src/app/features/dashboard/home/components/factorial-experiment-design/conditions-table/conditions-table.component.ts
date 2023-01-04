a1a5fa67bf566bfa2e3f392d47efc2abdb6737fd
Dan Oswalt <doswalt@carnegielearning.com>
2023-01-04 11:26:23 -0500

    update form and action button functionality

--- frontend/projects/upgrade/src/app/features/dashboard/home/components/factorial-experiment-design/conditions-table/conditions-table.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/home/components/factorial-experiment-design/conditions-table/conditions-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy, OnDestroy, EventEmitter, Output } from '@angular/core';
-import { BehaviorSubject, filter, Subscription } from 'rxjs';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { BehaviorSubject, filter, Subscription, take } from 'rxjs';
 import { ExperimentDesignStepperService } from '../../../../../../core/experiment-design-stepper/experiment-design-stepper.service';
 import { FactorialConditionTableRowData } from '../../../../../../core/experiment-design-stepper/store/experiment-design-stepper.model';
 
@@ -13,22 +14,29 @@ export class ConditionsTableComponent implements OnInit, OnDestroy {
   @Output() hide = new EventEmitter<boolean>();
 
   subscriptions: Subscription;
+  factorialConditionTableForm: FormGroup;
   factorialDesignData$ = this.experimentDesignStepperService.factorialDesignData$;
-  tableData$ = this.experimentDesignStepperService.factorialConditionTableData$;
+  tableData$ = new BehaviorSubject<FactorialConditionTableRowData[]>([]);
   tableEditIndex$ = this.experimentDesignStepperService.factorialConditionsTableEditIndex$;
   isFormLockedForEdit$ = this.experimentDesignStepperService.isFormLockedForEdit$;
   previousRowDataBehaviorSubject$ = new BehaviorSubject<FactorialConditionTableRowData>(null);
   columnHeaders = ['levelNameOne', 'levelNameTwo', 'alias', 'weight', 'include', 'actions'];
   equalWeightFlag = true;
+  showForm = false;
   factorOneHeader = 'factor1';
   factorTwoHeader = 'factor2';
 
-  constructor(private experimentDesignStepperService: ExperimentDesignStepperService) {}
+  constructor(
+    private experimentDesignStepperService: ExperimentDesignStepperService,
+    private _formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.experimentDesignStepperService.factorialConditionsEditModePreviousRowData$.subscribe(
       this.previousRowDataBehaviorSubject$
     );
+    this.experimentDesignStepperService.factorialConditionTableData$.subscribe(this.tableData$);
+    this.createForm();
   }
 
   ngAfterViewInit(): void {
@@ -45,27 +53,89 @@ export class ConditionsTableComponent implements OnInit, OnDestroy {
         this.factorOneHeader = designData.factors[0].factor;
         this.factorTwoHeader = designData.factors[1].factor;
       });
+
+    this.subscriptions = this.tableData$
+      .pipe(
+        filter((tableData) => !!tableData && tableData.length > 0),
+        take(1)
+      )
+      .subscribe((tableData) => {
+        tableData.forEach((tableDataRow) => {
+          const formControls = this._formBuilder.group({
+            levelNameOne: [tableDataRow.levelNameOne],
+            levelNameTwo: [tableDataRow.levelNameTwo],
+            alias: [tableDataRow.alias],
+            weight: [tableDataRow.weight],
+            include: [tableDataRow.include],
+          });
+
+          this.getFactorialConditions().push(formControls);
+        });
+
+        this.showForm = true;
+      });
   }
 
   ngOnDestroy(): void {
     this.subscriptions.unsubscribe();
+    this.resetEdit();
   }
+
+  createForm() {
+    this.factorialConditionTableForm = this._formBuilder.group({
+      factorialConditions: this._formBuilder.array([]),
+    });
+  }
+
+  resetPreviousRowDataOnEditCancel(previousRowData: FactorialConditionTableRowData, rowIndex: number) {
+    this.resetEdit();
+  }
+
+  resetEdit(): void {
+    this.experimentDesignStepperService.clearFactorialConditionTableEditModeDetails();
+  }
+
+  /* -- action button handlers -- */
 
   handleHideClick() {
     this.hide.emit(true);
   }
 
-  handleTableEditClick(rowIndex: number, rowData: FactorialConditionTableRowData) {
+  handleRowEditClick(rowData: FactorialConditionTableRowData, rowIndex: number) {
     this.experimentDesignStepperService.setFactorialConditionTableEditModeDetails(rowIndex, rowData);
+  }
+
+  handleRowEditDoneClick(rowIndex: number) {
+    const tableData = [...this.tableData$.value];
+    const formRow = this.getFactorialConditionsAt(rowIndex);
+
+    const alias = formRow.get('alias').value;
+    const weight = formRow.get('weight').value;
+    const include = formRow.get('include').value;
+
+    tableData[rowIndex] = { ...tableData[rowIndex], alias, weight, include };
+    this.tableData$.next(tableData);
+    this.experimentDesignStepperService.clearFactorialConditionTableEditModeDetails();
   }
 
   handleClear(rowIndex: number) {
     const previousRowData = this.previousRowDataBehaviorSubject$.value;
+    const formRow = this.getFactorialConditionsAt(rowIndex);
+
+    formRow.get('alias').setValue(previousRowData.alias, { emitEvent: false });
+    formRow.get('weight').setValue(previousRowData.weight, { emitEvent: false });
+    formRow.get('include').setValue(previousRowData.include, { emitEvent: false });
 
     this.resetPreviousRowDataOnEditCancel(previousRowData, rowIndex);
   }
 
-  resetPreviousRowDataOnEditCancel(previousRowData: FactorialConditionTableRowData, rowIndex: number) {
-    this.experimentDesignStepperService.clearFactorialConditionTableEditModeDetails();
+  /* -- convenience accessors --*/
+
+  getFactorialConditions(): FormArray {
+    return this.factorialConditionTableForm.get('factorialConditions') as FormArray;
+  }
+
+  getFactorialConditionsAt(rowIndex: number) {
+    return this.getFactorialConditions().at(rowIndex);
   }
 }
