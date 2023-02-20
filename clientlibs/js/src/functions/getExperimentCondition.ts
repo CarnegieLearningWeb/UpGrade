1075e4232d1f4a97d27eac541a29ec5c73fe6e43
RidhamShah <ridham.shah@ahduni.edu.in>
2023-02-21 00:02:46 +0530

    Updated new SDK (remove context from APIs data)

--- clientlibs/js/src/functions/getExperimentCondition.ts
+++ clientlibs/js/src/functions/getExperimentCondition.ts
@@ -1,21 +1,86 @@
-import { IExperimentAssignment } from 'upgrade_types';
+import { IExperimentAssignment, EXPERIMENT_TYPE } from 'upgrade_types';
+
+export class Assignment {
+  private _conditionCode: string;
+  private _conditionAlias: string;
+  private _experimentType: EXPERIMENT_TYPE;
+  private _assignedFactor: Record<string, { level: string; alias: string }>;
+
+  constructor(
+    conditionCode: string,
+    conditionAlias: string,
+    assignedFactor?: Record<string, { level: string; alias: string }>
+  ) {
+    this._conditionCode = conditionCode;
+    this._conditionAlias = conditionAlias;
+    this._experimentType = assignedFactor ? EXPERIMENT_TYPE.FACTORIAL : EXPERIMENT_TYPE.SIMPLE;
+    this._assignedFactor = assignedFactor;
+  }
+
+  public get conditionCode(): string {
+    return this._conditionCode;
+  }
+
+  public get conditionAlias(): string {
+    return this._conditionAlias;
+  }
+
+  public get experimentType(): EXPERIMENT_TYPE {
+    return this._experimentType;
+  }
+
+  public get factors(): string[] {
+    return this._experimentType === EXPERIMENT_TYPE.FACTORIAL ? Object.keys(this._assignedFactor) : null;
+  }
+
+  public getFactorAssignment(factor: string): FactorAssignment {
+    return this._experimentType === EXPERIMENT_TYPE.FACTORIAL
+      ? new FactorAssignment(this._assignedFactor[factor])
+      : null;
+  }
+}
+
+class FactorAssignment {
+  private _levelCode: string;
+  private _levelAlias: string;
+
+  constructor(assignedFactor: { level: string; alias: string }) {
+    this._levelCode = assignedFactor['level'] || null;
+    this._levelAlias = assignedFactor['alias'] || null;
+  }
+
+  public get levelCode(): string {
+    return this._levelCode;
+  }
+
+  public get levelAlias(): string {
+    return this._levelAlias;
+  }
+}
 
 export default function getExperimentCondition(
   experimentConditionData: IExperimentAssignment[],
-  site: string,
-  target?: string
-): IExperimentAssignment {
-  try {
-    if (experimentConditionData) {
-      const result = experimentConditionData.filter((data) =>
-        target ? data.target === target && data.site === site : data.site === site && !data.target
-      );
+  experimentPoint: string,
+  partitionId?: string
+): Assignment {
+  if (experimentConditionData) {
+    const result = experimentConditionData.filter((data) =>
+      partitionId
+        ? data.expId === partitionId && data.expPoint === experimentPoint
+        : data.expPoint === experimentPoint && !data.expId
+    );
 
-      return result.length ? result[0] : null;
+    if (result.length) {
+      const assignment = new Assignment(
+        result[0].assignedCondition.conditionCode,
+        result[0].assignedCondition.conditionCode,
+        result[0].assignedFactor
+      );
+      return assignment;
     } else {
       return null;
     }
-  } catch (error) {
-    throw new Error(error);
+  } else {
+    return null;
   }
 }
