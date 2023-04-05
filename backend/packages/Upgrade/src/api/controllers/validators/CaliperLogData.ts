7798f5de75046020ea77f6252423527b84ce605e
Jennifer Reddig <jreddig@carnegielearning.com>
2023-04-05 11:30:15 -0400

    update caliper data object

--- backend/packages/Upgrade/src/api/controllers/validators/CaliperLogData.ts
+++ backend/packages/Upgrade/src/api/controllers/validators/CaliperLogData.ts
@@ -1,12 +1,17 @@
 import { IsNotEmpty, IsDefined, IsString, IsJSON, IsObject } from 'class-validator';
-import { Attempt, CaliperActor, ScoreObject } from 'upgrade_types';
+import { Attempt, CaliperActor, ScoreObject, SUPPORTED_CALIPER_EVENTS, SUPPORTED_CALIPER_PROFILES } from 'upgrade_types';
 
 export class CaliperLogData {
 
   @IsDefined()
   @IsNotEmpty()
   @IsString()
-  public profile: string;
+  public profile: SUPPORTED_CALIPER_PROFILES;
+
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  public type: SUPPORTED_CALIPER_EVENTS;
 
   @IsDefined()
   @IsNotEmpty()
