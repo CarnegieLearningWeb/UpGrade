7a61f65e0c9da5a34f5ce130928750bb6af22543
Jennifer Reddig <jreddig@carnegielearning.com>
2023-05-15 16:11:48 -0400

    revert

--- backend/packages/Upgrade/src/api/controllers/validators/ExperimentAssignmentValidator.ts
+++ backend/packages/Upgrade/src/api/controllers/validators/ExperimentAssignmentValidator.ts
@@ -1,22 +1,11 @@
-import { IsNotEmpty, IsUUID, IsObject, IsEnum } from 'class-validator';
-import { EXPERIMENT_STATE } from 'upgrade_types';
+import { IsNotEmpty, IsDefined } from 'class-validator';
 
 export class ExperimentAssignmentValidator {
   @IsNotEmpty()
-  @IsUUID()
-  public experimentId: string;
-
-  @IsNotEmpty()
-  public site: string;
-
-  @IsNotEmpty()
+  @IsDefined()
   public userId: string;
 
-  @IsObject()
-  public userEnvironment: object;
-
-  @IsEnum(EXPERIMENT_STATE)
-  public state: EXPERIMENT_STATE;
-
+  @IsNotEmpty()
+  @IsDefined()
   public context: string;
 }
