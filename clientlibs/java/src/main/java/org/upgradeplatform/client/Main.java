a2c511f0810aa666605191f308feecfc699b3871
Jennifer Reddig <jreddig@carnegielearning.com>
2023-06-08 07:55:32 -0400

    handle patch

--- clientlibs/java/src/main/java/org/upgradeplatform/client/Main.java
+++ clientlibs/java/src/main/java/org/upgradeplatform/client/Main.java
@@ -21,94 +21,91 @@ import org.upgradeplatform.responsebeans.UserAliasResponse;
 import org.upgradeplatform.utils.Utils.MarkedDecisionPointStatus;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException, ExecutionException
-	{
-		final String baseUrl = "https://upgradeapi.qa-cli.com";
-		final String userId = UUID.randomUUID().toString();
-		final String site = "SelectSection";
+    public static void main(String[] args) throws InterruptedException, ExecutionException
+    {
+        final String baseUrl = "https://upgradeapi.qa-cli.com";
+        final String userId = UUID.randomUUID().toString();
+        final String site = "SelectSection";
 
-		String target = args.length > 0 ? args[0] : "graph_setup_linear_equation-1";
+        String target = args.length > 0 ? args[0] : "graph_setup_linear_equation-1";
 
-		try(ExperimentClient experimentClient = new ExperimentClient(userId, "BearerToken", baseUrl, Collections.emptyMap())){
-		    CompletableFuture<String> result = new CompletableFuture<>();
+        try(ExperimentClient experimentClient = new ExperimentClient(userId, "BearerToken", baseUrl, Collections.emptyMap())){
+            CompletableFuture<String> result = new CompletableFuture<>();
 
             System.out.println(prefix() + "initiating requests");
             experimentClient.init(new ResponseCallback<InitializeUser>() {
                 @Override
                 public void onSuccess(@NonNull InitializeUser t){
 
-                    List<String> schools = new ArrayList<String>();
+                    List<String> schools = new ArrayList<>();
                     schools.add("school1");
-                    Map<String, List<String>> group = new HashMap<String, List<String>>();
+                    Map<String, List<String>> group = new HashMap<>();
                     group.put("schoolid", schools);
 
                     System.out.println(prefix() + "setting group membership");
                     experimentClient.setGroupMembership(group, new ResponseCallback<ExperimentUser>(){
                         @Override
                         public void onSuccess(@NonNull ExperimentUser expResult){
-                            System.out.println("success updating groups");
-                        }
-                        @Override
-                        public void onError(@NonNull ErrorResponse error){
-                            System.out.println("error updating groups " + error);
-                        }
-                    });
+                            System.out.println(prefix() + "success updating groups; setting working group");
 
-
-                    System.out.println(prefix() + "setting working group");
-                    Map<String, String> workingGroup = new HashMap<String, String>();
-                    workingGroup.put("schoolId", "school1");
-                    experimentClient.setWorkingGroup(workingGroup, new ResponseCallback<ExperimentUser>(){
-                        @Override
-                        public void onSuccess(@NonNull ExperimentUser expResult){
-                            System.out.println("success updating working groups");
-                        }
-                        @Override
-                        public void onError(@NonNull ErrorResponse error){
-                            System.out.println("error updating working groups " + error);
-                        }
-                    });
-
-
-                    System.out.println(prefix() + "setting user aliases");
-                    List<String> altIds = new ArrayList<String>();
-                    altIds.add(UUID.randomUUID().toString());
-                    experimentClient.setAltUserIds(altIds, new ResponseCallback<UserAliasResponse>(){
-                        @Override
-                        public void onSuccess(@NonNull UserAliasResponse t) {
-                            System.out.println("success updating user aliases");
-                        }
-                        @Override
-                        public void onError(@NonNull ErrorResponse error){
-                            System.out.println("error updating user aliases " + error);
-                        }
-
-                    });
-
-                    System.out.println(prefix() + "getting conditions");
-                    experimentClient.getExperimentCondition("assign-prog", site, target, new ResponseCallback<ExperimentsResponse>(){
-                        @Override
-                        public void onSuccess(@NonNull ExperimentsResponse expResult){
-                            Condition condition = expResult.getAssignedCondition();
-                            String code = condition == null ? null : condition.getCondition();
-                            experimentClient.markExperimentPoint(site, target, code, MarkedDecisionPointStatus.CONDITION_APPLIED, new ResponseCallback<MarkExperimentPoint>(){
+                            Map<String, String> workingGroup = new HashMap<>();
+                            workingGroup.put("schoolId", "school1");
+                            experimentClient.setWorkingGroup(workingGroup, new ResponseCallback<ExperimentUser>(){
                                 @Override
-                                public void onSuccess(@NonNull MarkExperimentPoint markResult){
-                                    result.complete("marked " + code + ": " + markResult.toString());
-                                }
+                                public void onSuccess(@NonNull ExperimentUser expResult){
+                                    System.out.println(prefix() + "success updating working groups; setting user aliases");
 
+                                    List<String> altIds = new ArrayList<>();
+                                    altIds.add(UUID.randomUUID().toString());
+                                    experimentClient.setAltUserIds(altIds, new ResponseCallback<UserAliasResponse>(){
+                                        @Override
+                                        public void onSuccess(@NonNull UserAliasResponse uar) {
+                                            System.out.println(prefix() + "success updating user aliases; getting conditions");
+
+                                            experimentClient.getExperimentCondition("assign-prog", site, target, new ResponseCallback<ExperimentsResponse>(){
+                                                @Override
+                                                public void onSuccess(@NonNull ExperimentsResponse expResult){
+                                                    System.out.println(prefix() + "success getting condition; marking");
+
+                                                    Condition condition = expResult.getAssignedCondition();
+                                                    String code = condition == null ? null : condition.getCondition();
+                                                    experimentClient.markExperimentPoint(site, target, code, MarkedDecisionPointStatus.CONDITION_APPLIED, new ResponseCallback<MarkExperimentPoint>(){
+                                                        @Override
+                                                        public void onSuccess(@NonNull MarkExperimentPoint markResult){
+                                                            result.complete("marked " + code + ": " + markResult.toString());
+                                                        }
+
+                                                        @Override
+                                                        public void onError(@NonNull ErrorResponse error){
+                                                            result.complete("error marking " + code + ": " + error.toString());
+                                                        }
+                                                    });
+                                                }
+
+                                                @Override
+                                                public void onError(@NonNull ErrorResponse error){
+                                                    result.complete(error.toString());
+                                                }
+
+                                            });
+                                        }
+                                        @Override
+                                        public void onError(@NonNull ErrorResponse error){
+                                            result.complete(error.toString());
+                                        }
+
+                                    });
+                                }
                                 @Override
                                 public void onError(@NonNull ErrorResponse error){
-                                    result.complete("error marking " + code + ": " + error.toString());
+                                    result.complete(error.toString());
                                 }
                             });
                         }
-
                         @Override
                         public void onError(@NonNull ErrorResponse error){
                             result.complete(error.toString());
                         }
-
                     });
                 }
 
@@ -121,10 +118,10 @@ public class Main {
             System.out.println(prefix() + result.getNow("not complete yet"));
             String rs = result.get();
             System.out.println(prefix() + rs);
-		}
-	}
+        }
+    }
 
-	private static final String prefix() {
-	    return "on thread " + Thread.currentThread().getName() + " at " + System.currentTimeMillis() + ": ";
-	}
+    private static final String prefix() {
+        return "on thread " + Thread.currentThread().getName() + " at " + System.currentTimeMillis() + ": ";
+    }
 }
