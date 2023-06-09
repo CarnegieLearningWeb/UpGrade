638d9fcab73cfa12b490aeb96646112d23e4c1e5
Dan Oswalt <doswalt@carnegielearning.com>
2023-06-09 15:38:47 -0400

    add readme and stuff

--- clientlibs/libTesters/client-lib-tester-frontend/src/app/app-config.ts
+++ clientlibs/libTesters/client-lib-tester-frontend/src/app/app-config.ts
@@ -2,20 +2,11 @@ import { UpgradeClient as UpgradeClient_Local } from 'upgrade_client_local';
 import { UpgradeClient as UpgradeClient_1_1_17 } from 'upgrade_client_1_1_17';
 import { UpgradeClient as UpgradeClient_3_0_18 } from 'upgrade_client_3_0_18';
 import { UpgradeClient as UpgradeClient_4_2_0 } from 'upgrade_client_4_2_0';
+import { MOCK_APP_NAMES } from '../../../shared/constants';
 
-// export declare type GenericUpgradeClient =
-//   | UpgradeClient_Local
-//   | UpgradeClient_1_1_17
-//   | UpgradeClient_3_0_18
-//   | UpgradeClient_4_2_0;
-
-// the client type described here just means the client library constructor (and not an actual client library instance)
-// this is because we want to allow the mock client app code to be able to instantiate UpgradeClient itself
-// export interface ClientLibraryRef {
-//   version: string;
-//   client: new (...args: any[]) => GenericUpgradeClient;
-// }
-
+/**
+ * Frontend config
+ */
 export interface ClientLibraryRef {
   version: string;
   language: 'ts' | 'java';
@@ -51,8 +42,4 @@ export const availableApiHostUrls = [
   { value: 'https://upgradeapi.qa-cli.com', viewValue: 'STAGING: https://upgradeapi.qa-cli.com' },
 ];
 
-export const MOCK_APP_NAMES = {
-  BDAY_APP: 'Birthday App Frontend',
-  BDAY_APP_BACKEND: 'Birthday App Backend',
-  PORTAL_APP: 'Mock Portal',
-};
+export const availableFrontendMockApps = [MOCK_APP_NAMES.BDAY_APP, MOCK_APP_NAMES.PORTAL_APP];
