a71042744548f7d39ebf2d08d0dfaf16f3d923b0
jreddig <97543136+jreddig@users.noreply.github.com>
2023-06-12 10:22:14 -0400

    pull updates from v3 (#875)

--- clientlibs/java/src/main/java/org/upgradeplatform/utils/APIService.java
+++ clientlibs/java/src/main/java/org/upgradeplatform/utils/APIService.java
@@ -10,8 +10,9 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
+import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
-import org.glassfish.jersey.client.HttpUrlConnectorProvider;
 
 
 public class APIService implements AutoCloseable{
@@ -35,7 +36,7 @@ public class APIService implements AutoCloseable{
 	}
 
 	public static Client createClient(Map<String,Object> properties) {
-		Client client = ClientBuilder.newClient();
+		Client client = ClientBuilder.newClient(new ClientConfig().connectorProvider(new ApacheConnectorProvider()));
 		client.property(ClientProperties.CONNECT_TIMEOUT, 3000);
 		client.property(ClientProperties.READ_TIMEOUT,    3000);
 		properties.entrySet().stream()
@@ -57,7 +58,6 @@ public class APIService implements AutoCloseable{
 				.request(MediaType.APPLICATION_JSON)
 				.header("Authorization", "Bearer "+this.authToken)
 				.header("Session-Id", this.sessionId)
-				.property(HttpUrlConnectorProvider.SET_METHOD_WORKAROUND, true)
 				.async();
 	}
 
