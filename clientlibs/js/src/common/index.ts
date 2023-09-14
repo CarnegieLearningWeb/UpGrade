eddbae515db332ed523756517d2ced00a312faae
RidhamShah <ridham.shah@ahduni.edu.in>
2023-09-14 10:28:34 +0530

    Solve mark and assignment issue

--- clientlibs/js/src/common/index.ts
+++ clientlibs/js/src/common/index.ts
@@ -1,4 +1,4 @@
-import { IExperimentAssignmentv5 } from "../../../../types/src";
+import { IExperimentAssignmentv5, PAYLOAD_TYPE } from 'upgrade_types';
 
 export function rotateAssignmentList(assignment: IExperimentAssignmentv5) {
   if (assignment.assignedCondition.length > 1) {
@@ -10,7 +10,25 @@ export function rotateAssignmentList(assignment: IExperimentAssignmentv5) {
   return assignment;
 }
 
-export function findExperimentAssignmentBySiteAndTarget(site: string, target: string, experimentAssignmentData: IExperimentAssignmentv5[]): IExperimentAssignmentv5 {
-  const assignment = experimentAssignmentData.find((assignment) => assignment.site === site && assignment.target === target);
-  return assignment;
-};
+export function findExperimentAssignmentBySiteAndTarget(
+  site: string,
+  target: string,
+  experimentAssignmentData: IExperimentAssignmentv5[]
+): IExperimentAssignmentv5 {
+  const assignment = experimentAssignmentData.find(
+    (assignment) => assignment.site === site && assignment.target === target
+  );
+  return (
+    assignment || {
+      site: site,
+      target: target,
+      assignedCondition: [
+        {
+          conditionCode: null,
+          payload: null,
+          id: null,
+        },
+      ],
+    }
+  );
+}
