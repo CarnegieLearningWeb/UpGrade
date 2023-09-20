de5860c6d8b4e8288743b132620d4bd0c0f501b2
danoswaltCL <97542869+danoswaltCL@users.noreply.github.com>
2023-09-20 08:49:51 -0400

    rearrange lines

--- package-scripts.js
+++ package-scripts.js
@@ -5,16 +5,16 @@
 
 function versionBump(bumpType) {
   return `
-    echo "root:" && npm version ${bumpType} &&        
+    NEW_VERSION=$(jq -r '.version' package.json) &&
+    echo "npm version sometimes takes a minute to apply tag apparently, give it a sec" &&
+    echo "root:" && npm version ${bumpType} &&       
     echo "types:" && cd types && npm version ${bumpType} --git-tag-version=false &&
     echo "frontend:" && cd ../frontend && npm version ${bumpType} --git-tag-version=false &&
     echo "backend-root:" && cd ../backend && npm version ${bumpType} --git-tag-version=false &&
     echo "scheduler:" && cd packages/Scheduler && npm version ${bumpType} --git-tag-version=false &&
     echo "backend:" && cd ../Upgrade && npm version ${bumpType} --git-tag-version=false &&
     echo "js clientlib:" && cd ../../../clientlibs/js && npm version ${bumpType} --git-tag-version=false &&
-    NEW_VERSION=$(jq -r '.version' package.json) &&
-    echo "java clientlib:" && cd ../java && mvn versions:set -DnewVersion=${process.env.NEW_VERSION} &&
-    cd ../../`;
+    echo "java clientlib:" && cd ../java && mvn versions:set -DnewVersion=${process.env.NEW_VERSION}`;
 }
 
 module.exports = {
