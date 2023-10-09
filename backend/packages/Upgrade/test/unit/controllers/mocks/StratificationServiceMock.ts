cd76470f621c5c2a9afb74d520057656c7e2cb48
Yagnik <yagnikhingrajiya56@gmail.com>
2023-10-09 18:01:40 +0530

    SRS backend controller and repo unit testcases

---
+++ backend/packages/Upgrade/test/unit/controllers/mocks/StratificationServiceMock.ts
@@ -0,0 +1,29 @@
+import { Service } from 'typedi';
+import { UpgradeLogger } from '../../../../src/lib/logger/UpgradeLogger';
+import { StratificationInputValidator } from 'src/api/controllers/validators/StratificationValidator';
+
+@Service()
+export default class StratificationServieMock {
+  public getAllStratification(logger: UpgradeLogger): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public getStratificationByFactor(factor: string, logger: UpgradeLogger): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public getCSVDataByFactor(factor: string, logger: UpgradeLogger): Promise<any> {
+    return Promise.resolve({});
+  }
+
+  public deleteStratification(factor: string, logger: UpgradeLogger): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public insertStratification(
+    userStratificationData: StratificationInputValidator[],
+    logger: UpgradeLogger
+  ): Promise<[]> {
+    return Promise.resolve([]);
+  }
+}
