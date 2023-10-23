54bc4c6e0fcc923b900a9aa1d85917812b4e760d
RidhamShah <ridham.shah@ahduni.edu.in>
2023-10-23 17:48:24 +0530

    updated code and solved testcases

--- frontend/projects/upgrade/src/app/core/notifications/notification.service.ts
+++ frontend/projects/upgrade/src/app/core/notifications/notification.service.ts
@@ -1,8 +1,4 @@
-import { Inject, Injectable, NgZone } from '@angular/core';
-import {
-  MatLegacySnackBar as MatSnackBar,
-  MatLegacySnackBarConfig as MatSnackBarConfig,
-} from '@angular/material/legacy-snack-bar';
+import { Inject, Injectable } from '@angular/core';
 import { NotificationsService, NotificationType } from 'angular2-notifications';
 import { ENV, Environment } from '../../../environments/environment-types';
 
@@ -10,54 +6,37 @@ import { ENV, Environment } from '../../../environments/environment-types';
   providedIn: 'root',
 })
 export class NotificationService {
-  constructor(
-    private readonly snackBar: MatSnackBar,
-    private readonly zone: NgZone,
-    private _notifications: NotificationsService,
-    @Inject(ENV) private environment: Environment
-  ) {}
+  constructor(private _notifications: NotificationsService, @Inject(ENV) private environment: Environment) {}
 
-  default(message: string) {
-    this.show(message, {
-      duration: 2000,
-      panelClass: 'default-notification-overlay',
-    });
+  showInfo(message: string) {
+    const temp = {
+      type: NotificationType.Info,
+      title: 'Info',
+      content: message,
+      animate: 'fromRight',
+      timeOut: 2000,
+    };
+    this.showSnackBar(temp);
   }
 
-  info(message: string) {
-    this.show(message, {
-      duration: 2000,
-      panelClass: 'info-notification-overlay',
-    });
-  }
-
-  success(message: string, duration?: number) {
-    this.show(message, {
-      duration: duration || 2000,
-      panelClass: 'success-notification-overlay',
-    });
-  }
-
-  warn(message: string) {
-    this.show(message, {
-      duration: 2500,
-      panelClass: 'warning-notification-overlay',
-    });
-  }
-
-  error(message: string) {
-    this.show(message, {
-      duration: 1500,
-      panelClass: 'error-notification-overlay',
-    });
+  showWarning(message: string) {
+    const temp = {
+      type: NotificationType.Warn,
+      title: 'Warning',
+      content: message,
+      animate: 'fromRight',
+      timeOut: 2500,
+    };
+    this.showSnackBar(temp);
   }
 
   showError(message: string) {
     const temp = {
       type: NotificationType.Error,
-      title: 'ERROR.',
+      title: 'Error.',
       content: message,
       animate: 'fromRight',
+      timeOut: 1500,
     };
     this.showSnackBar(temp);
   }
@@ -68,6 +47,7 @@ export class NotificationService {
       title: 'Success',
       content: message,
       animate: 'fromRight',
+      timeOut: 4000,
     };
     this.showSnackBar(temp);
   }
@@ -79,11 +59,5 @@ export class NotificationService {
     if (!this.environment.production) {
       this._notifications.create(snackBarDetail.title, snackBarDetail.content, snackBarDetail.type, snackBarDetail);
     }
-  }
-
-  private show(message: string, configuration: MatSnackBarConfig) {
-    // Need to open snackBar from Angular zone to prevent issues with its position per
-    // https://stackoverflow.com/questions/50101912/snackbar-position-wrong-when-use-errorhandler-in-angular-5-and-material
-    this.zone.run(() => this.snackBar.open(message, null, configuration));
   }
 }
