85f0076e98da1acc203ff12ca8b88c47016f4f04
danoswaltCL <97542869+danoswaltCL@users.noreply.github.com>
2023-11-21 14:21:02 -0500

    add logic to check login as destination, improve specs

--- frontend/projects/upgrade/src/app/core/meta-reducers/clear-state.reducer.spec.ts
+++ frontend/projects/upgrade/src/app/core/meta-reducers/clear-state.reducer.spec.ts
@@ -1,5 +1,5 @@
 import { createReducer } from '@ngrx/store';
-import { actionLogoutSuccess } from '../auth/store/auth.actions';
+import { actionLogoutSuccess, actionLoginStart } from '../auth/store/auth.actions';
 import { LocalStorageService } from '../local-storage/local-storage.service';
 import { ThemeOptions } from '../settings/store/settings.model';
 import { clearState } from './clear-state.reducer';
@@ -11,11 +11,7 @@ describe('clearState', () => {
       theme: ThemeOptions.DARK_THEME,
     },
     auth: {
-      isLoggedIn: false,
-      isAuthenticating: false,
-      user: null,
-      googleCredential: null,
-      redirectUrl: '/home',
+      redirectUrl: '/test',
     },
   };
 
@@ -23,7 +19,7 @@ describe('clearState', () => {
     LocalStorageService.prototype.removeItem = jest.fn();
   });
 
-  it('should reset settings state and clear localStorage settings', () => {
+  it('should reset settings and auth state except for theme and redirectUrl on actionLogoutSuccess, and also clear localStorage settings', () => {
     const reducer = createReducer(mockState);
     const metaReducer = clearState(reducer);
     const expectedResetState = {
@@ -37,7 +33,7 @@ describe('clearState', () => {
         isAuthenticating: false,
         user: null,
         googleCredential: null,
-        redirectUrl: '/home',
+        redirectUrl: '/test',
       },
     };
 
@@ -47,5 +43,38 @@ describe('clearState', () => {
       Object.keys(ExperimentLocalStorageKeys).length
     );
     expect(newState).toEqual(expectedResetState);
+  });
+
+  it('should NOT reset anything or clear localStorage if action is anything but actionLogoutSuccess', () => {
+    const mockWithNonDefaultState = {
+      ...mockState,
+      auth: {
+        ...mockState.auth,
+        isLoggedIn: true,
+      },
+    };
+    const reducer = createReducer(mockState);
+    const metaReducer = clearState(reducer);
+    const expectedResetState = {
+      settings: {
+        theme: ThemeOptions.DARK_THEME,
+        toCheckAuth: null,
+        toFilterMetric: null,
+      },
+      auth: {
+        isLoggedIn: true,
+        isAuthenticating: false,
+        user: null,
+        googleCredential: null,
+        redirectUrl: '/test',
+      },
+    };
+
+    const newState = metaReducer(mockWithNonDefaultState, actionLoginStart());
+
+    expect(LocalStorageService.prototype.removeItem).not.toHaveBeenCalledTimes(
+      Object.keys(ExperimentLocalStorageKeys).length
+    );
+    expect(newState).not.toEqual(expectedResetState);
   });
 });
