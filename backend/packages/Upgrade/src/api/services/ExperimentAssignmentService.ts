57adc921bfec6d0d40ba8df13a194bc1a3dc7568
Pratik Prajapati <33730817+ppratikcr7@users.noreply.github.com>
2023-12-13 21:21:37 +0530

    resolving bug for null partitionId stored in enrolment docs resulting in wrong enrolment counts (#1181)

--- backend/packages/Upgrade/src/api/services/ExperimentAssignmentService.ts
+++ backend/packages/Upgrade/src/api/services/ExperimentAssignmentService.ts
@@ -329,7 +329,7 @@ export class ExperimentAssignmentService {
           await this.updateEnrollmentExclusion(
             userDoc,
             experiment,
-            selectedExperimentDP[0],
+            selectedExperimentDP,
             {
               individualEnrollment: individualEnrollments,
               individualExclusion: individualExclusions,
