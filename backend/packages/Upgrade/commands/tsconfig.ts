913773a69439aa070bde009fcffdf4290f8e8e4f
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2024-01-29 20:45:20 +0530

    :bug: Fixing tsconfig.build.json (#1271)
    Removing references from tsconfig.build.json for deployment

--- backend/packages/Upgrade/commands/tsconfig.ts
+++ backend/packages/Upgrade/commands/tsconfig.ts
@@ -3,10 +3,11 @@ import * as path from 'path';
 
 import * as tsconfig from '../tsconfig.json';
 
-const content: any = tsconfig;
+const content: any = JSON.parse(JSON.stringify(tsconfig)); // Clone tsconfig.json
 content.compilerOptions.outDir = 'dist';
 content.include = ['src/**/*', 'custom.d.ts'];
 content.compilerOptions.paths.upgrade_types = ['./types'];
+delete content.references;
 
 const filePath = path.join(process.cwd(), 'tsconfig.build.json');
 jsonfile.writeFile(filePath, content, { spaces: 2 }, (err) => {
