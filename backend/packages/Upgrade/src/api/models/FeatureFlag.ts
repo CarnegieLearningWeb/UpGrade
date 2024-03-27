69e9523b64e58497640360ffb7ea023dbf96795a
Benjamin Blanchard <bblanchard@carnegielearning.com>
2024-03-27 15:50:53 -0400

    Feature flag inclusion/exclusion db migration

--- backend/packages/Upgrade/src/api/models/FeatureFlag.ts
+++ backend/packages/Upgrade/src/api/models/FeatureFlag.ts
@@ -1,8 +1,10 @@
-import { Column, Entity, PrimaryColumn, OneToMany } from 'typeorm';
+import { Column, Entity, PrimaryColumn, OneToMany, OneToOne } from 'typeorm';
 import { IsNotEmpty, ValidateNested } from 'class-validator';
 import { BaseModel } from './base/BaseModel';
 import { Type } from 'class-transformer';
 import { FlagVariation } from './FlagVariation';
+import { FeatureFlagSegmentInclusion } from './FeatureFlagSegmentInclusion';
+import { FeatureFlagSegmentExclusion } from './FeatureFlagSegmentExclusion';
 
 @Entity()
 export class FeatureFlag extends BaseModel {
@@ -29,4 +31,12 @@ export class FeatureFlag extends BaseModel {
   @ValidateNested()
   @Type(() => FlagVariation)
   public variations: FlagVariation[];
+
+  @OneToOne(() => FeatureFlagSegmentInclusion, (featureFlagSegmentInclusion) => featureFlagSegmentInclusion.featureFlag)
+  @Type(() => FeatureFlagSegmentInclusion)
+  public featureFlagSegmentInclusion: FeatureFlagSegmentInclusion;
+
+  @OneToOne(() => FeatureFlagSegmentExclusion, (featureFlagSegmentExclusion) => featureFlagSegmentExclusion.featureFlag)
+  @Type(() => FeatureFlagSegmentExclusion)
+  public featureFlagSegmentExclusion: FeatureFlagSegmentExclusion;
 }
