a281aad31becd56f45c82e7ba82864cea1bde365
RidhamShah <ridham.shah@ahduni.edu.in>
2024-04-12 12:03:52 +0530

    Stepper update to show metric as per context

--- types/src/Experiment/interfaces.ts
+++ types/src/Experiment/interfaces.ts
@@ -110,6 +110,7 @@ export interface IExperimentSortParams {
 
 export interface IMetricUnit {
   key: string | string[];
+  context?: string[];
   children?: IMetricUnit[];
   metadata?: { type: IMetricMetaData };
   allowedData?: string[];
