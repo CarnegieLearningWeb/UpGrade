665e5dd08fcc93a4bd8e4940561c85648d0031eb
danoswaltCL <97542869+danoswaltCL@users.noreply.github.com>
2024-05-01 07:55:47 -0400

    just feature flag root page and header

--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-page.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-page.component.ts
@@ -1,21 +1,12 @@
 import { Component } from '@angular/core';
-import {
-  CommonRootPageComponent,
-  CommonSectionCardListComponent,
-} from '../../../../../shared-standalone-component-lib/components';
+import { CommonRootPageComponent } from '../../../../../shared-standalone-component-lib/components';
 import { FeatureFlagRootPageHeaderComponent } from './feature-flag-root-page-header/feature-flag-root-page-header.component';
-import { FeatureFlagRootSectionCardListComponent } from './feature-flag-root-section-card-list/feature-flag-root-section-card-list.component';
 
 @Component({
   selector: 'app-feature-flag-root-page',
   standalone: true,
   templateUrl: './feature-flag-root-page.component.html',
   styleUrl: './feature-flag-root-page.component.scss',
-  imports: [
-    CommonRootPageComponent,
-    CommonSectionCardListComponent,
-    FeatureFlagRootPageHeaderComponent,
-    FeatureFlagRootSectionCardListComponent,
-  ],
+  imports: [CommonRootPageComponent, FeatureFlagRootPageHeaderComponent],
 })
 export class FeatureFlagRootPageComponent {}
--- frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/index.ts
+++ frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/index.ts
@@ -1,13 +1,4 @@
 import { CommonRootPageHeaderContainerComponent } from './common-root-page-header-container/common-root-page-header-container.component';
 import { CommonRootPageComponent } from './common-root-page/common-root-page.component';
-import { CommonSectionCardHeaderComponent } from './common-section-card-header/common-section-card-header.component';
-import { CommonSectionCardListComponent } from './common-section-card-list/common-section-card-list.component';
-import { CommonSectionCardComponent } from './common-section-card/common-section-card.component';
 
-export {
-  CommonRootPageComponent,
-  CommonSectionCardComponent,
-  CommonSectionCardListComponent,
-  CommonRootPageHeaderContainerComponent,
-  CommonSectionCardHeaderComponent,
-};
+export { CommonRootPageComponent, CommonRootPageHeaderContainerComponent };
