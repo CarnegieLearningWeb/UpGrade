6337c93e758ad1223bb3c3672f0b0de594bdd687
Zack Lee <zlee@carnegielearning.com>
2024-05-08 10:29:49 -0400

    Move and update the CommonSectionCardList component

--- frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/index.ts
+++ frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/index.ts
@@ -1,4 +1,4 @@
-import { CommonSectionCardListComponent } from '../common-section-card-list/common-section-card-list.component';
+import { CommonSectionCardListComponent } from './common-section-card-list/common-section-card-list.component';
 import { CommonRootPageHeaderContainerComponent } from './common-root-page-header-container/common-root-page-header-container.component';
 import { CommonRootPageComponent } from './common-root-page/common-root-page.component';
 
