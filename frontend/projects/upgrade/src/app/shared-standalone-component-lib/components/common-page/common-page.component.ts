04b9581366dd9008b46db992eef1b74a057291bd
Zack Lee <zlee@carnegielearning.com>
2024-05-15 14:04:22 -0400

    Merge CommonDetailsPage and CommonRootPage into CommonPage component

--- frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/common-page/common-page.component.ts
+++ frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/common-page/common-page.component.ts
@@ -1,29 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { CommonRootPageHeaderContainerComponent } from '../common-root-page-header-container/common-root-page-header-container.component';
 
 /**
  * A component nested in this component can be displayed in the **header** or **content** slot.
- * The **header** slot should contain a component that wraps a **app-common-root-page-header**.
+ * The **header** slot should contain a component that wraps a **app-common-page-header**.
  * The **content** slot should contain a component that wraps a **app-common-section-card-list**.
  * Simply nest the component and add the slot name as an attribute.
  *
  * Example usage:
  *
  * ```
- * <app-common-root-page>
+ * <app-common-page>
  *   <div header>Hi I'm in the header slot</div>
  *   <div content>Hi I'm in the content slot</div>
- * </app-common-root-page>
+ * </app-common-page>
  * ```
  */
 @Component({
-  selector: 'app-common-root-page',
+  selector: 'app-common-page',
   standalone: true,
-  imports: [CommonModule, TranslateModule, CommonRootPageHeaderContainerComponent],
-  templateUrl: './common-root-page.component.html',
-  styleUrl: './common-root-page.component.scss',
+  imports: [CommonModule, TranslateModule],
+  templateUrl: './common-page.component.html',
+  styleUrl: './common-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CommonRootPageComponent {}
+export class CommonPageComponent {}
