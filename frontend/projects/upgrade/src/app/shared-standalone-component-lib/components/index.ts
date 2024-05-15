d7fb312147938cdb3d0871ff11b2babcd4b9699a
Zack Lee <90279765+zackcl@users.noreply.github.com>
2024-05-15 14:31:43 -0400

    Merge CommonDetailsPage and CommonRootPage into CommonPage component (#1537)

--- frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/index.ts
+++ frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/index.ts
@@ -1,13 +1,11 @@
-import { CommonSectionCardListComponent } from './common-section-card-list/common-section-card-list.component';
+import { CommonPageComponent } from './common-page/common-page.component';
 import { CommonRootPageHeaderContainerComponent } from './common-root-page-header-container/common-root-page-header-container.component';
-import { CommonRootPageComponent } from './common-root-page/common-root-page.component';
-import { CommonDetailsPageComponent } from './common-details-page/common-details-page.component';
 import { CommonDetailsPageHeaderContainerComponent } from './common-details-page-header-container/common-details-page-header-container.component';
+import { CommonSectionCardListComponent } from './common-section-card-list/common-section-card-list.component';
 
 export {
-  CommonRootPageComponent,
+  CommonPageComponent,
   CommonRootPageHeaderContainerComponent,
-  CommonSectionCardListComponent,
-  CommonDetailsPageComponent,
   CommonDetailsPageHeaderContainerComponent,
+  CommonSectionCardListComponent,
 };
