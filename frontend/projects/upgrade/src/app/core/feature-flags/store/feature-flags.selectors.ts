7d0e954af35b07f2f9994796e6dfd0dec60f405d
Yagnik <yagnikhingrajiya56@gmail.com>
2024-05-16 20:38:29 +0530

    added service changes and shifted some logic to selectors

--- frontend/projects/upgrade/src/app/core/feature-flags/store/feature-flags.selectors.ts
+++ frontend/projects/upgrade/src/app/core/feature-flags/store/feature-flags.selectors.ts
@@ -7,21 +7,43 @@ export const selectFeatureFlagsState = createFeatureSelector<FeatureFlagState>('
 
 export const selectAllFeatureFlags = createSelector(selectFeatureFlagsState, selectAll);
 
+export const selectAllFeatureFlagsSortedByDate = createSelector(selectAllFeatureFlags, (featureFlags) => {
+  if (!featureFlags) {
+    return [];
+  }
+  return featureFlags.sort((a, b) => {
+    const d1 = new Date(a.createdAt);
+    const d2 = new Date(b.createdAt);
+    return d1 < d2 ? 1 : d1 > d2 ? -1 : 0;
+  });
+});
+
 export const selectIsLoadingFeatureFlags = createSelector(
   selectFeatureFlagsState,
   (state) => state.isLoadingFeatureFlags
 );
 
+export const selectIsInitialFeatureFlagsLoading = createSelector(
+  selectIsLoadingFeatureFlags,
+  selectAllFeatureFlagsSortedByDate,
+  (isLoading, featureFlags) => !isLoading || !!featureFlags.length
+);
+
 export const selectSelectedFeatureFlag = createSelector(
   selectRouterState,
   selectFeatureFlagsState,
-  ({ state: { params } }, featureFlagState) =>
-    featureFlagState.entities[params.flagId] ? featureFlagState.entities[params.flagId] : undefined
+  ({ state: { params } }, featureFlagState) => featureFlagState.entities[params.flagId]
 );
 
 export const selectSkipFlags = createSelector(selectFeatureFlagsState, (state) => state.skipFlags);
 
 export const selectTotalFlags = createSelector(selectFeatureFlagsState, (state) => state.totalFlags);
+
+export const selectIsAllFlagsFetched = createSelector(
+  selectSkipFlags,
+  selectTotalFlags,
+  (skipFlags, totalFlags) => skipFlags === totalFlags
+);
 
 export const selectSearchKey = createSelector(selectFeatureFlagsState, (state) => state.searchKey);
 
