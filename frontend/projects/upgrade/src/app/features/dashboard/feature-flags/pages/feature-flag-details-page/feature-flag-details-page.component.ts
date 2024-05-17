90828ea2dc8437b160f08bcc82d8dc2821afef08
danoswaltCL <doswalt@carnegielearning.com>
2024-05-17 16:37:26 -0400

    removed imports

--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonPageComponent } from '../../../../../shared-standalone-component-lib/components';
 import { FeatureFlagDetailsPageHeaderComponent } from './feature-flag-details-page-header/feature-flag-details-page-header.component';
-import { FeatureFlagDetailsPageContentComponent } from './feature-flag-details-page-content/feature-flag-details-page-content.component';
 
 @Component({
   selector: 'app-feature-flag-details-page',
   standalone: true,
   templateUrl: './feature-flag-details-page.component.html',
   styleUrl: './feature-flag-details-page.component.scss',
-  imports: [CommonPageComponent, FeatureFlagDetailsPageHeaderComponent, FeatureFlagDetailsPageContentComponent],
+  imports: [CommonPageComponent, FeatureFlagDetailsPageHeaderComponent],
 })
 export class FeatureFlagDetailsPageComponent {}
--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-page.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonPageComponent } from '../../../../../shared-standalone-component-lib/components';
 import { FeatureFlagRootPageHeaderComponent } from './feature-flag-root-page-header/feature-flag-root-page-header.component';
-import { FeatureFlagRootPageContentComponent } from './feature-flag-root-page-content/feature-flag-root-page-content.component';
 
 @Component({
   selector: 'app-feature-flag-root-page',
   standalone: true,
   templateUrl: './feature-flag-root-page.component.html',
   styleUrl: './feature-flag-root-page.component.scss',
-  imports: [CommonPageComponent, FeatureFlagRootPageHeaderComponent, FeatureFlagRootPageContentComponent],
+  imports: [CommonPageComponent, FeatureFlagRootPageHeaderComponent],
 })
 export class FeatureFlagRootPageComponent {}
