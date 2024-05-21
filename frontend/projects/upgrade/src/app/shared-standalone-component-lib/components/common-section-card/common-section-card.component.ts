13699db77e8585e4e1af9fc146d7d1cedad5568c
danoswaltCL <97542869+danoswaltCL@users.noreply.github.com>
2024-05-21 13:50:16 -0400

    common section card and basic root section card with search header (#1552)
    * common section card and basic root section card with search header
    
    * remove dupe html
    
    * forgot to rename the actual content slot name from content to section card

--- frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/common-section-card/common-section-card.component.ts
+++ frontend/projects/upgrade/src/app/shared-standalone-component-lib/components/common-section-card/common-section-card.component.ts
@@ -4,7 +4,8 @@ import { TranslateModule } from '@ngx-translate/core';
 
 /**
  * A component nested in this component can be displayed in the **header**, **content** or **footer** slot.
- * The **header** slot should contain a component that wraps a **app-common-section-card-header**.
+ * The **header-left** slot should contain a search or title component.
+ * The **header-right** slot should contain an action button component.
  * The **content** slot should contain a component that wraps a **app-common-section-card-content**.
  * The **footer** slot should contain a component that wraps a **app-common-section-card-footer**.
  * Simply nest the component and add the slot name as an attribute.
@@ -13,9 +14,10 @@ import { TranslateModule } from '@ngx-translate/core';
  *
  * ```
  * <app-common-section-card>
- *   <div header>Hi I'm in the header slot</div>
+ *   <div header-left>Hi I'm in the header left slot</div>
+ *   <div header-right>Hi I'm in the header right slot</div>
  *   <div content>Hi I'm in the content slot</div>
- *   <div footer>Hi I'm in the content slot</div>
+ *   <div footer>Hi I'm in the footer slot</div>
  * </app-common-section-card>
  * ```
  */
