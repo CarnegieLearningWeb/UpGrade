42820e75e97373bc46007b0c700a08c7bcf4bdfe
Zack Lee <90279765+zackcl@users.noreply.github.com>
2024-05-22 17:00:28 -0400

    Implement Common Section Card Action Buttons (#1569)

--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-section-card/feature-flag-root-section-card.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-section-card/feature-flag-root-section-card.component.ts
@@ -2,10 +2,13 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
   CommonSectionCardComponent,
   CommonSectionCardSearchHeaderComponent,
+  CommonSectionCardActionButtonsComponent,
 } from '../../../../../../shared-standalone-component-lib/components';
 import { FeatureFlagsService } from '../../../../../../core/feature-flags/feature-flags.service';
 import { AsyncPipe, JsonPipe, NgIf } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { IMenuButtonItem } from 'upgrade_types';
 
 @Component({
   selector: 'app-feature-flag-root-section-card',
@@ -13,10 +16,12 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   imports: [
     CommonSectionCardComponent,
     CommonSectionCardSearchHeaderComponent,
+    CommonSectionCardActionButtonsComponent,
     AsyncPipe,
     JsonPipe,
     NgIf,
     MatProgressSpinnerModule,
+    TranslateModule,
   ],
   templateUrl: './feature-flag-root-section-card.component.html',
   styleUrl: './feature-flag-root-section-card.component.scss',
@@ -27,7 +32,21 @@ export class FeatureFlagRootSectionCardComponent {
   allFeatureFlags$ = this.featureFlagService.allFeatureFlags$;
   isAllFlagsFetched$ = this.featureFlagService.isAllFlagsFetched$;
 
-  constructor(private featureFlagService: FeatureFlagsService) {}
+  menuButtonItems: IMenuButtonItem[] = [
+    {
+      name: this.translateService.instant('feature-flags.import-feature-flag.text'),
+      disabled: false
+    },
+    {
+      name: this.translateService.instant('feature-flags.export-all-feature-flags.text'),
+      disabled: true
+    }
+  ];
+
+  constructor(
+    private featureFlagService: FeatureFlagsService,
+    private translateService: TranslateService
+  ) {}
 
   ngOnInit() {
     this.featureFlagService.fetchFeatureFlags();
@@ -37,4 +56,17 @@ export class FeatureFlagRootSectionCardComponent {
     console.log('searchString', searchString);
     // this.featureFlagService.setSearchString(searchString);
   }
+
+  onAddFeatureFlagButtonClick() {
+    console.log('onAddFeatureFlagButtonClick');
+  }
+
+  onMenuButtonItemClick(menuButtonItemName: string) {
+    console.log('onMenuButtonItemClick:', menuButtonItemName);
+  }
+
+  onSectionCardExpandChange(isSectionCardExpanded: boolean) {
+    console.log('onSectionCardExpandChange:', isSectionCardExpanded);
+  }
+
 }
