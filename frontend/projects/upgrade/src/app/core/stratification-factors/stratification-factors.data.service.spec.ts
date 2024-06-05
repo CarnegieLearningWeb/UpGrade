3f63e950dabc4501f95f7dd32ef23342c7e77796
JayMehta11 <jay.m@ahduni.edu.in>
2024-06-05 10:40:03 +0530

    stratification-UI multiple CSV upload testcase solved

--- frontend/projects/upgrade/src/app/core/stratification-factors/stratification-factors.data.service.spec.ts
+++ frontend/projects/upgrade/src/app/core/stratification-factors/stratification-factors.data.service.spec.ts
@@ -37,12 +37,10 @@ describe('StratificationFactorsDataService', () => {
 
   describe('#importStratificationFactors', () => {
     it('should post the importStratificationFactors http observable', () => {
-      const mockStratificationFactors = { files: [{ name: 'factor1' }, { name: 'factor2' }] };
+      const mockStratificationFactors = [{ name: 'factor1' }, { name: 'factor2' }];
       const expectedUrl = mockEnvironment.api.stratification;
-
       service.importStratificationFactors(mockStratificationFactors);
-
-      expect(mockHttpClient.post).toHaveBeenCalledWith(expectedUrl, mockStratificationFactors);
+      expect(mockHttpClient.post).toHaveBeenCalledWith(expectedUrl, { files: mockStratificationFactors });
     });
   });
 
