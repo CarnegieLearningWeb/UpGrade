8e2e775769da6745022c9d1dd6e265784db49c72
danoswaltCL <doswalt@carnegielearning.com>
2024-06-05 21:48:43 -0400

    add both include and exclude headers with common header tableRowCount added also

--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page-content/feature-flag-exclusions-section-card/feature-flag-exclusions-section-card.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page-content/feature-flag-exclusions-section-card/feature-flag-exclusions-section-card.component.ts
@@ -1,12 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonSectionCardComponent } from '../../../../../../../shared-standalone-component-lib/components';
+import {
+  CommonSectionCardComponent,
+  CommonSectionCardTitleHeaderComponent,
+} from '../../../../../../../shared-standalone-component-lib/components';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-feature-flag-exclusions-section-card',
   standalone: true,
-  imports: [CommonSectionCardComponent],
+  imports: [CommonSectionCardComponent, CommonSectionCardTitleHeaderComponent, CommonModule, TranslateModule],
   templateUrl: './feature-flag-exclusions-section-card.component.html',
   styleUrl: './feature-flag-exclusions-section-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FeatureFlagExclusionsSectionCardComponent {}
+export class FeatureFlagExclusionsSectionCardComponent {
+  tableRowCount$ = of(1);
+}
