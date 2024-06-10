26c6c9699f0963f9ef86c3e79ee3688e79984abd
Yagnik Hingrajiya <50392803+Yagnik56@users.noreply.github.com>
2024-06-10 18:35:03 +0530

    action button for include & exclude list section cards (#1636)

--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page-content/feature-flag-exclusions-section-card/feature-flag-exclusions-section-card.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page-content/feature-flag-exclusions-section-card/feature-flag-exclusions-section-card.component.ts
@@ -1,20 +1,46 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
+  CommonSectionCardActionButtonsComponent,
   CommonSectionCardComponent,
   CommonSectionCardTitleHeaderComponent,
 } from '../../../../../../../shared-standalone-component-lib/components';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { of } from 'rxjs';
+import { IMenuButtonItem } from 'upgrade_types';
 
 @Component({
   selector: 'app-feature-flag-exclusions-section-card',
   standalone: true,
-  imports: [CommonSectionCardComponent, CommonSectionCardTitleHeaderComponent, CommonModule, TranslateModule],
+  imports: [
+    CommonSectionCardComponent,
+    CommonSectionCardTitleHeaderComponent,
+    CommonSectionCardActionButtonsComponent,
+    CommonModule,
+    TranslateModule,
+  ],
   templateUrl: './feature-flag-exclusions-section-card.component.html',
   styleUrl: './feature-flag-exclusions-section-card.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FeatureFlagExclusionsSectionCardComponent {
   tableRowCount$ = of(1);
+  menuButtonItems: IMenuButtonItem[] = [
+    { name: 'Edit', disabled: false },
+    { name: 'Delete', disabled: false },
+  ];
+  isSectionCardExpanded = true;
+
+  addExcludeListClicked() {
+    console.log('add Exclude List Clicked');
+  }
+
+  onMenuButtonItemClick(event) {
+    console.log('Menu button Clicked');
+    console.log(event);
+  }
+
+  onSectionCardExpandChange(isSectionCardExpanded: boolean) {
+    this.isSectionCardExpanded = isSectionCardExpanded;
+  }
 }
