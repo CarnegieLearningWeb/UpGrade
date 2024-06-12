41be2f85f3a3f0aa22cf226129b13430e602e1bc
Yagnik Hingrajiya <50392803+Yagnik56@users.noreply.github.com>
2024-06-12 20:54:53 +0530

    feature flag delete functionality (#1644)
    Co-authored-by: danoswaltCL <97542869+danoswaltCL@users.noreply.github.com>

--- frontend/projects/upgrade/src/app/shared/services/common-dialog.service.ts
+++ frontend/projects/upgrade/src/app/shared/services/common-dialog.service.ts
@@ -3,6 +3,7 @@ import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { MatConfirmDialogComponent } from '../components/mat-confirm-dialog/mat-confirm-dialog.component';
 import { AddFeatureFlagModalComponent } from '../../features/dashboard/feature-flags/modals/add-feature-flag-modal/add-feature-flag-modal.component';
 import { CommonModalConfig } from '../../shared-standalone-component-lib/components/common-modal/common-modal-config';
+import { DeleteFeatureFlagModalComponent } from '../../features/dashboard/feature-flags/modals/delete-feature-flag-modal/delete-feature-flag-modal.component';
 
 @Injectable({
   providedIn: 'root',
@@ -31,5 +32,26 @@ export class DialogService {
       disableClose: true,
     };
     return this.dialog.open(AddFeatureFlagModalComponent, config);
+  }
+
+  openDeleteFeatureFlagModal(flagName: string, flagId: string) {
+    const commonModalConfig: CommonModalConfig = {
+      title: 'Delete Feature Flag',
+      primaryActionBtnLabel: 'Delete',
+      primaryActionBtnColor: 'primary',
+      cancelBtnLabel: 'Cancel',
+    };
+    const config: MatDialogConfig = {
+      data: {
+        ...commonModalConfig,
+        flagName: flagName,
+        flagId: flagId,
+      },
+      width: '670px',
+      height: '390px',
+      autoFocus: 'input',
+      disableClose: true,
+    };
+    return this.dialog.open(DeleteFeatureFlagModalComponent, config);
   }
 }
