8de7936908982a4886e9a14e7c81a8201ce95791
RidhamShah <ridham.shah@ahduni.edu.in>
2024-06-13 10:52:05 +0530

    solve global.ts errors
    (cherry picked from commit f254123ec183dae2c4e044e92e563badd412c5cb)

--- backend/packages/Upgrade/src/typeorm-typedi-extensions/global.ts
+++ backend/packages/Upgrade/src/typeorm-typedi-extensions/global.ts
@@ -1,4 +1,4 @@
-import { Constructable } from 'typedi';
+import Constructable from 'typedi';
 import { DataSource, EntityManager, ObjectLiteral, Repository } from 'typeorm';
 
 // CustomRepository with Entity and Manager
@@ -9,7 +9,7 @@ export type CustomRepositoryMetadata<T extends ObjectLiteral> =
       target: CustomRepository<T>;
       entity: T;
     }
-  | { target: Constructable<any>; entity: undefined }; // For repositories without any Entity
+  | { target: Constructable; entity: undefined }; // For repositories without any Entity
 
 export type DataSourceMetadata = { name: string; dataSource: DataSource };
 
