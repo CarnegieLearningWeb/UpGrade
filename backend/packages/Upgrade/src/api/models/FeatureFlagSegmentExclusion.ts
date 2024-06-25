851a2c8135ef3a35f4ed0e5ba709b5f2adeb8dfc
Vivek Fitkariwala <vivekfitkariwala@gmail.com>
2024-06-25 16:14:29 +0530

    Updating model

--- backend/packages/Upgrade/src/api/models/FeatureFlagSegmentExclusion.ts
+++ backend/packages/Upgrade/src/api/models/FeatureFlagSegmentExclusion.ts
@@ -6,11 +6,15 @@ import { Segment } from './Segment';
 @Entity()
 export class FeatureFlagSegmentExclusion extends BaseModel {
   @PrimaryColumn('uuid')
+  public segmentId: string;
+
   @OneToOne(() => Segment, (segment) => segment.featureFlagSegmentExclusion, { onDelete: 'CASCADE' })
   @JoinColumn()
   public segment: Segment;
 
   @PrimaryColumn('uuid')
+  public featureFlagId: string;
+
   @OneToOne(() => FeatureFlag, (featureFlag) => featureFlag.featureFlagSegmentExclusion, {
     onDelete: 'CASCADE',
   })
--- backend/packages/Upgrade/src/api/models/FeatureFlagSegmentInclusion.ts
+++ backend/packages/Upgrade/src/api/models/FeatureFlagSegmentInclusion.ts
@@ -6,11 +6,15 @@ import { Segment } from './Segment';
 @Entity()
 export class FeatureFlagSegmentInclusion extends BaseModel {
   @PrimaryColumn('uuid')
+  public segmentId: string;
+
   @OneToOne(() => Segment, (segment) => segment.featureFlagSegmentInclusion, { onDelete: 'CASCADE' })
   @JoinColumn()
   public segment: Segment;
 
   @PrimaryColumn('uuid')
+  public featureFlagId: string;
+
   @OneToOne(() => FeatureFlag, (featureFlag) => featureFlag.featureFlagSegmentInclusion, {
     onDelete: 'CASCADE',
   })
