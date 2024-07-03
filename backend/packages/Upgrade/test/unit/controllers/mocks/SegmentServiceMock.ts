e429643aa2db6d55a93276f1d47b1e2b36f80c2a
pratik <ppratik.cr7@gmail.com>
2024-07-03 15:53:47 +0530

    adding segmentcontroller unit test cases and resolved other small issues in unit tests

---
+++ backend/packages/Upgrade/test/unit/controllers/mocks/SegmentServiceMock.ts
@@ -0,0 +1,52 @@
+import { Service } from 'typedi';
+
+@Service()
+export default class ExcludeServiceMock {
+  public getAllSegments(): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public getAllSegmentWithStatus(): Promise<[]> {
+    return Promise.resolve([]);
+  }
+  
+  public getSegmentById(id: string): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public getSegmentWithStatusById(id: string): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public getSingleSegmentWithStatus(id: string): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public upsertSegment(): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public importSegments(): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public deleteSegment(id: string): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public validateSegments(): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public exportSegments(id: string): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public exportSegment(id: string): Promise<[]> {
+    return Promise.resolve([]);
+  }
+
+  public exportSegmentCSV(id: string): Promise<[]> {
+    return Promise.resolve([]);
+  }
+}
