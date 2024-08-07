d49da4ce654ab4fbda0d8fdb7522690453d0bfdb
Zack Lee <zlee@carnegielearning.com>
2024-08-07 18:44:14 +0900

    Update getUserDoc to return requestedUserId even when userDoc is not found

--- backend/packages/Upgrade/src/api/services/ExperimentUserService.ts
+++ backend/packages/Upgrade/src/api/services/ExperimentUserService.ts
@@ -295,16 +295,12 @@ export class ExperimentUserService {
     return this.userRepository.save(newDocument);
   }
 
-  public async getUserDoc(experimentUserId, logger): Promise<RequestedExperimentUser> {
+  public async getUserDoc(experimentUserId: string, logger: UpgradeLogger): Promise<RequestedExperimentUser | null> {
     try {
       const experimentUserDoc = await this.getOriginalUserDoc(experimentUserId, logger);
-      if (experimentUserDoc) {
-        const userDoc = { ...experimentUserDoc, requestedUserId: experimentUserId };
-        logger.info({ message: 'Got the user doc', details: userDoc });
-        return userDoc;
-      } else {
-        return null;
-      }
+      const userDoc = { ...experimentUserDoc, requestedUserId: experimentUserId };
+      logger.info({ message: 'Got the user doc', details: userDoc });
+      return userDoc;
     } catch (error) {
       logger.error({ message: `Error in getting user doc for user => ${experimentUserId}`, error });
       return null;
