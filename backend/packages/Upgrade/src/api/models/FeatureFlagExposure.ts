2f3b11b5a0ed2ce0f0bd5085e8d78814c37213d8
Benjamin Blanchard <bblanchard@carnegielearning.com>
2024-08-29 14:31:16 -0400

    revert migration (only track unique exposures)

--- backend/packages/Upgrade/src/api/models/FeatureFlagExposure.ts
+++ backend/packages/Upgrade/src/api/models/FeatureFlagExposure.ts
@@ -1,18 +1,14 @@
-import { Entity, Index, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Index, ManyToOne } from 'typeorm';
 import { BaseModel } from './base/BaseModel';
 import { FeatureFlag } from './FeatureFlag';
 import { ExperimentUser } from './ExperimentUser';
 
 @Entity()
 export class FeatureFlagExposure extends BaseModel {
-  @PrimaryGeneratedColumn('uuid')
-  public id: string;
-
   @Index()
-  @ManyToOne(() => FeatureFlag, { onDelete: 'CASCADE' })
+  @ManyToOne(() => FeatureFlag, { onDelete: 'CASCADE', primary: true })
   public featureFlag: FeatureFlag;
-
   @Index()
-  @ManyToOne(() => ExperimentUser, { onDelete: 'CASCADE' })
+  @ManyToOne(() => ExperimentUser, { onDelete: 'CASCADE', primary: true })
   public experimentUser: ExperimentUser;
 }
