654fce9cacff0dd522389793331c44493415182a
Zack Lee <zlee@carnegielearning.com>
2024-09-05 21:08:54 +0900

    Support multiple google client and service account IDs

--- frontend/settings.env.js
+++ frontend/settings.env.js
@@ -2,5 +2,5 @@ module.exports = {
   API_BASE_URL: process.env.API_BASE_URL || ' ',
   BASE_HREF_PREFIX: process.env.BASE_HREF_PREFIX || ' ',
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || ' ',
-  SERVICE_ACCOUNT_ID: process.env.SERVICE_ACCOUNT_ID || ' ',
+  GOOGLE_SERVICE_ACCOUNT_ID: process.env.GOOGLE_SERVICE_ACCOUNT_ID || ' ',
 };
