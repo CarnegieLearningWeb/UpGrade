ae23312b3a91c1e0d248cee3a3e1e5f0a4081cb8
Zack Lee <zlee@carnegielearning.com>
2024-09-05 22:12:17 +0900

    Remove GOOGLE_SERVICE_ACCOUNT_ID from settings.env.js and update comments for .env example files

--- frontend/settings.env.js
+++ frontend/settings.env.js
@@ -2,5 +2,4 @@ module.exports = {
   API_BASE_URL: process.env.API_BASE_URL || ' ',
   BASE_HREF_PREFIX: process.env.BASE_HREF_PREFIX || ' ',
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || ' ',
-  GOOGLE_SERVICE_ACCOUNT_ID: process.env.GOOGLE_SERVICE_ACCOUNT_ID || ' ',
 };
