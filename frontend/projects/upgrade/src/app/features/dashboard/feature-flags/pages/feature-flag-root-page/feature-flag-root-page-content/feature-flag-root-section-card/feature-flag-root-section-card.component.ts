d2d2a3340d081ffac987160eb9d8a30ba374ee3b
Zack Lee <90279765+zackcl@users.noreply.github.com>
2024-09-11 23:15:40 +0900

    Display 'No matching feature flags found' message when no flags found from search (#1906)
    * Display 'No matching feature flags found' message when no flags found from search
    
    * Refactor isSearchActive$ to derive from store

--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-page-content/feature-flag-root-section-card/feature-flag-root-section-card.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-root-page/feature-flag-root-page-content/feature-flag-root-section-card/feature-flag-root-section-card.component.ts
@@ -13,7 +13,7 @@ import { FLAG_SEARCH_KEY, IMenuButtonItem } from 'upgrade_types';
 import { RouterModule } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 import { DialogService } from '../../../../../../../shared/services/common-dialog.service';
-import { Observable, Subscription, map } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { FeatureFlag } from '../../../../../../../core/feature-flags/store/feature-flags.model';
 import { CommonSearchWidgetSearchParams } from '../../../../../../../shared-standalone-component-lib/components/common-section-card-search-header/common-section-card-search-header.component';
 import {
@@ -49,6 +49,7 @@ export class FeatureFlagRootSectionCardComponent {
   searchKey$ = this.featureFlagService.searchKey$;
   searchParams$ = this.featureFlagService.searchParams$;
   selectRootTableState$ = this.featureFlagService.selectRootTableState$;
+  isSearchActive$: Observable<boolean> = this.searchString$.pipe(map((searchString) => !!searchString));
 
   featureFlagFilterOption = [
     FLAG_SEARCH_KEY.ALL,
