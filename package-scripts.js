298ab180d2ede5483ee9a2c9900512d1cb362b1e
Benjamin Blanchard <bblanchard@carnegielearning.com>
2024-10-04 15:45:02 -0400

    add prerelease version and script calls

--- package-scripts.js
+++ package-scripts.js
@@ -5,14 +5,14 @@
 
 function versionBump(bumpType) {
   return `
-    echo "root:" && npm version ${bumpType} --git-tag-version=false &&
+    echo "root:" && npm version ${bumpType} --git-tag-version=false --preid=prerelease &&
     NEW_VERSION=$(jq -r '.version' package.json) &&       
-    echo "types:" && cd types && npm version ${bumpType} --git-tag-version=false &&
-    echo "frontend:" && cd ../frontend && npm version ${bumpType} --git-tag-version=false &&
-    echo "backend-root:" && cd ../backend && npm version ${bumpType} --git-tag-version=false &&
-    echo "scheduler:" && cd packages/Scheduler && npm version ${bumpType} --git-tag-version=false &&
-    echo "backend:" && cd ../Upgrade && npm version ${bumpType} --git-tag-version=false &&
-    echo "js clientlib:" && cd ../../../clientlibs/js && npm version ${bumpType} --git-tag-version=false &&
+    echo "types:" && cd types && npm version ${bumpType} --git-tag-version=false --preid=prerelease &&
+    echo "frontend:" && cd ../frontend && npm version ${bumpType} --git-tag-version=false --preid=prerelease &&
+    echo "backend-root:" && cd ../backend && npm version ${bumpType} --git-tag-version=false --preid=prerelease &&
+    echo "scheduler:" && cd packages/Scheduler && npm version ${bumpType} --git-tag-version=false --preid=prerelease &&
+    echo "backend:" && cd ../Upgrade && npm version ${bumpType} --git-tag-version=false --preid=prerelease &&
+    echo "js clientlib:" && cd ../../../clientlibs/js && npm version ${bumpType} --git-tag-version=false --preid=prerelease &&
     echo "java clientlib:" && cd ../java && mvn versions:set -DnewVersion=$NEW_VERSION`;
 }
 
@@ -23,6 +23,10 @@ module.exports = {
       patch: versionBump('patch'),
       minor: versionBump('minor'),
       major: versionBump('major'),
+      prerelease: versionBump('prerelease'),
+      premajor: versionBump('premajor'),
+      preminor: versionBump('preminor'),
+      prepatch: versionBump('prepatch'),
     },
   },
 };
