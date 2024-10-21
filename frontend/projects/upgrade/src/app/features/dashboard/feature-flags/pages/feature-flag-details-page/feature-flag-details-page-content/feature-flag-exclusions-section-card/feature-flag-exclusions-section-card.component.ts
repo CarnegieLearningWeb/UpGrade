2c7ce14169a38bfe3a74ea4793b2dcb7b5efa90a
Yagnik <yagnikhingrajiya56@gmail.com>
2024-10-21 21:07:08 +0530

    remove console log

--- frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page-content/feature-flag-exclusions-section-card/feature-flag-exclusions-section-card.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/feature-flags/pages/feature-flag-details-page/feature-flag-details-page-content/feature-flag-exclusions-section-card/feature-flag-exclusions-section-card.component.ts
@@ -65,7 +65,6 @@ export class FeatureFlagExclusionsSectionCardComponent {
   }
 
   onMenuButtonItemClick(event, flag: FeatureFlag) {
-    console.log('Menu Button Item Clicked:', event);
     switch (event) {
       case 'Import Include List':
         this.dialogService
