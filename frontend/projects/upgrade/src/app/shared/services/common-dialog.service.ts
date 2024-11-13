7be5fc7f762297972024e5dbad87063293b68890
Yagnik Hingrajiya <50392803+Yagnik56@users.noreply.github.com>
2024-11-13 20:20:22 +0530

    fix header and moved overflow to table body to make it scrollable (#2109)
    Co-authored-by: Zack Lee <90279765+zackcl@users.noreply.github.com>

--- frontend/projects/upgrade/src/app/shared/services/common-dialog.service.ts
+++ frontend/projects/upgrade/src/app/shared/services/common-dialog.service.ts
@@ -156,6 +156,7 @@ export class DialogService {
     const config: MatDialogConfig = {
       data: commonModalConfig,
       width: ModalSize.STANDARD,
+      autoFocus: true,
       disableClose: true,
     };
     return this.dialog.open(UpsertFeatureFlagModalComponent, config);
