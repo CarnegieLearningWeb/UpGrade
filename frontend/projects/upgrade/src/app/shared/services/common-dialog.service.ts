1f65c2cfd4d6b95dee65c649f04acfb2bb29820f
RidhamShah <ridham.shah@ahduni.edu.in>
2024-11-18 11:08:33 +0530

    solved feature-flag name field focus issue

--- frontend/projects/upgrade/src/app/shared/services/common-dialog.service.ts
+++ frontend/projects/upgrade/src/app/shared/services/common-dialog.service.ts
@@ -156,7 +156,7 @@ export class DialogService {
     const config: MatDialogConfig = {
       data: commonModalConfig,
       width: ModalSize.STANDARD,
-      autoFocus: true,
+      autoFocus: 'input',
       disableClose: true,
     };
     return this.dialog.open(UpsertFeatureFlagModalComponent, config);
