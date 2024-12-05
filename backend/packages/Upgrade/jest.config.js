7e5d87b65988d52bf9bf2903dfdb5d3cbc4aaeff
danoswaltCL <doswalt@carnegielearning.com>
2024-12-05 17:07:05 -0500

    try moduleDirectories in jest config

--- backend/packages/Upgrade/jest.config.js
+++ backend/packages/Upgrade/jest.config.js
@@ -78,7 +78,7 @@ module.exports = {
 
     // An array of directory names to be searched recursively up from the requiring module's location
     moduleDirectories: [
-      '<rootDir>/../../../types/node_modules'
+      '../../../types/node_modules'
     ],
 
     // An array of file extensions your modules use
@@ -93,7 +93,7 @@ module.exports = {
 
     // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
     moduleNameMapper: {
-        "^upgrade_types$": '<rootDir>/../../../types'
+        "^upgrade_types$": '../../../types'
     },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
