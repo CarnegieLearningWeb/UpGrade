381aae41738b73bb09ccbf598115aa6777c2a0cb
Vivek Fitkariwala <vivekfitkariwala@Viveks-MacBook-Pro.local>
2025-02-15 13:42:45 +0530

    ⬆️ Updating node version

--- backend/packages/Upgrade/commands/banner.ts
+++ backend/packages/Upgrade/commands/banner.ts
@@ -1,12 +1,12 @@
-import chalk from 'chalk';
 import figlet from 'figlet';
 
 figlet.text(process.argv[2], (error: any, data: any) => {
   if (error) {
     return process.exit(1);
   }
-
-  console.log(chalk.blue(data));
-  console.log('');
-  return process.exit(0);
+  import('chalk').then((chalk) => {
+    chalk.default.blue(data);
+    console.log('');
+    return process.exit(0);
+  });
 });
