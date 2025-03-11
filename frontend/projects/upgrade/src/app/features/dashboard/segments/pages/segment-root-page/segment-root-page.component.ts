508ca3b59ccc1dda2b54067df0b6f85e4064c4ec
Zack Lee <zlee@carnegielearning.com>
2025-03-11 20:17:57 +0900

    Rename SegmentsRootPageComponent and SegmentsDetailsPageComponent to SegmentRootPageComponent and SegmentDetailsPageComponent

---
+++ frontend/projects/upgrade/src/app/features/dashboard/segments/pages/segment-root-page/segment-root-page.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segment-root-page',
+  imports: [],
+  templateUrl: './segment-root-page.component.html',
+  styleUrl: './segment-root-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SegmentRootPageComponent {}
