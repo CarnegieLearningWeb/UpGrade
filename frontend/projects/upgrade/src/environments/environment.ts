d7f643dd9aa4e2ce0d15972238bedbca6db97a19
danoswaltCL <doswalt@carnegielearning.com>
2025-03-12 12:03:01 -0400

    add a note in environment.ts

--- frontend/projects/upgrade/src/environments/environment.ts
+++ frontend/projects/upgrade/src/environments/environment.ts
@@ -1,7 +1,9 @@
 // The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses 'environment.ts', but if you do
+// The build system defaults to the dev environment which uses 'environment.local.ts', but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
+
+// This file is NOT used anymore for any actual builds, so don't bother changing the values here, use 'environment.local.ts'.
 
 export const environment = {
   appName: 'UpGrade',
