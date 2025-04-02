67df1de95a642ec7286e8d71d26680edb6e4cf3a
Benjamin Blanchard <bblanchard@carnegielearning.com>
2025-04-02 11:10:49 -0400

    add endpoint to all envs

--- frontend/projects/upgrade/src/environments/environment.local.example.ts
+++ frontend/projects/upgrade/src/environments/environment.local.example.ts
@@ -72,6 +72,7 @@ export const environment: Environment = {
     getVersion: '/version',
     contextMetaData: '/experiments/contextMetaData',
     segments: '/segments',
+    globalSegments: '/segments/global',
     getPaginatedSegments: '/segments/paginated',
     validateSegments: '/segments/validation',
     validateSegmentsImport: '/segments/import/validation',
