41c0f4b282d165d6a60a453d46736ed822ff6b12
Zack Lee <90279765+zackcl@users.noreply.github.com>
2025-04-12 04:36:59 +0900

    Implement Segment Import List Modal (#2368)
    * Implement Segment Import List Modal
    
    * Update importSegmentList URL to match backend
    
    * Update segmentIdto parentSegmentId for importSegmentList

--- frontend/projects/upgrade/src/environments/environment.prod.ts
+++ frontend/projects/upgrade/src/environments/environment.prod.ts
@@ -71,6 +71,7 @@ export const environment: Environment = {
     validateSegments: '/segments/validation',
     validateSegmentsImport: '/segments/import/validation',
     importSegments: '/segments/import',
+    importSegmentList: '/segments/list/import',
     exportSegments: '/segments/export/json',
     exportSegmentCSV: '/segments/export/csv',
     addSegmentList: '/segments/list',
