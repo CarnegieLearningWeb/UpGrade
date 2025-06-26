0689f757b0ea38a6a78112ee6ef3e90bd1276804
Zack Lee <zlee@carnegielearning.com>
2025-06-26 18:41:11 +0900

    Implement experiment root section card table component

--- frontend/projects/upgrade/src/app/core/experiments/store/experiments.selectors.ts
+++ frontend/projects/upgrade/src/app/core/experiments/store/experiments.selectors.ts
@@ -65,7 +65,7 @@ export const selectSearchExperimentParams = createSelector(
 );
 
 export const selectRootTableState = createSelector(
-  selectAllExperimentFromState,
+  selectAllExperiment,
   selectSearchExperimentParams,
   (tableData, searchParams) => ({
     tableData,
