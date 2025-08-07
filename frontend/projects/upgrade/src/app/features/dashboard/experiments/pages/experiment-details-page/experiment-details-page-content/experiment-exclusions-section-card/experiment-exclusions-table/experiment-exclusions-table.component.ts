916f86993d34bd84c4db37f71eb33711b281787b
Zack Lee <zlee@carnegielearning.com>
2025-08-07 22:11:12 +0900

    Implement experiment exclude list table component

--- frontend/projects/upgrade/src/app/features/dashboard/experiments/pages/experiment-details-page/experiment-details-page-content/experiment-exclusions-section-card/experiment-exclusions-table/experiment-exclusions-table.component.ts
+++ frontend/projects/upgrade/src/app/features/dashboard/experiments/pages/experiment-details-page/experiment-details-page-content/experiment-exclusions-section-card/experiment-exclusions-table/experiment-exclusions-table.component.ts
@@ -1,14 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+import { ExperimentService } from '../../../../../../../../core/experiments/experiments.service';
+import { ParticipantListRowActionEvent } from '../../../../../../../../core/feature-flags/store/feature-flags.model';
+import { LIST_FILTER_MODE } from 'upgrade_types';
+import { CommonDetailsParticipantListTableComponent } from '../../../../../../../../shared-standalone-component-lib/components/common-details-participant-list-table/common-details-participant-list-table.component';
 
 @Component({
   selector: 'app-experiment-exclusions-table',
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonDetailsParticipantListTableComponent, CommonModule, TranslateModule],
   templateUrl: './experiment-exclusions-table.component.html',
   styleUrl: './experiment-exclusions-table.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ExperimentExclusionsTableComponent {
-  // TODO: Implement exclusions table functionality using CommonDetailsParticipantListTableComponent
+  @Input() actionsDisabled?: boolean = false;
+  tableType = LIST_FILTER_MODE.EXCLUSION;
+  dataSource$ = this.experimentService.selectExperimentExclusions$;
+  isLoading$ = this.experimentService.isLoadingExperiment$;
+  @Output() rowAction = new EventEmitter<ParticipantListRowActionEvent>();
+
+  constructor(private experimentService: ExperimentService) {}
+
+  onRowAction(event: ParticipantListRowActionEvent): void {
+    this.rowAction.emit(event);
+  }
 }
