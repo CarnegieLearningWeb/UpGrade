923003ae91d9d89ff703a9f148986064362546a8
Zack Lee <zlee@carnegielearning.com>
2025-09-19 01:40:13 +0900

    Update demo-app.js script and add Docker build support

--- frontend/projects/upgrade/src/assets/js/demo-app.js
+++ frontend/projects/upgrade/src/assets/js/demo-app.js
@@ -5,88 +5,80 @@ if (window.self !== window.top) {
   const getElementById = (id) => {
     switch (id) {
       // Home
-      case 'upgrade-logo-link':
-        return document.querySelector('div.logo a.logo-link');
+      case 'login-with-google-button':
+        return document.querySelector('div.login-container button.google-sign-in-btn');
       case 'experiments-tab':
         return document.querySelectorAll('div.list-item-container a.nav-item')[0];
       case 'signout-button':
-        return document.querySelector('mat-list.user-list a.logout-link');
+        return document.querySelector('span.mat-list-item-content a.logout-link');
       case 'add-experiment-button':
-        return document.querySelectorAll('mat-card.mat-mdc-card button.mat-mdc-unelevated-button')[1];
+        return document.querySelectorAll('mat-card.mat-card button.mat-flat-button')[1];
 
       // Experiment Stepper - Overview Step
       case 'experiment-stepper-overview-name':
-        return document.querySelectorAll('form.experiment-overview input.mat-mdc-input-element')[0];
+        return document.querySelectorAll('form.experiment-overview input.mat-input-element')[0];
       case 'experiment-stepper-overview-description':
-        return document.querySelectorAll('form.experiment-overview input.mat-mdc-input-element')[1];
+        return document.querySelectorAll('form.experiment-overview input.mat-input-element')[1];
       case 'experiment-stepper-overview-app-context':
-        return document.querySelectorAll('form.experiment-overview div.mat-mdc-select-value')[0];
+        return document.querySelectorAll('form.experiment-overview div.mat-select-value')[0];
       case 'experiment-stepper-overview-unit-of-assignment':
-        return document.querySelectorAll('form.experiment-overview div.mat-mdc-select-value')[1];
+        return document.querySelectorAll('form.experiment-overview div.mat-select-value')[1];
       case 'experiment-stepper-overview-consistency-rule':
-        return document.querySelectorAll('form.experiment-overview div.mat-mdc-select-value')[2];
+        return document.querySelectorAll('form.experiment-overview div.mat-select-value')[2];
       case 'experiment-stepper-overview-next-button':
-        return document.querySelectorAll('div.new-experiment-modal button.mat-mdc-raised-button')[1];
+        return document.querySelectorAll('div.new-experiment-modal button.mat-raised-button')[1];
 
       // Experiment Stepper - Design Step
       case 'experiment-stepper-design-add-decision-point-button':
-        return document.querySelector('form.experiment-design button.add-decision-point');
+        return document.querySelector('form.experiment-design button.add-partition');
       case 'experiment-stepper-design-decision-points-row1-site':
-        return document.querySelectorAll('mat-table.decision-point-table input.mat-mdc-input-element')[0];
+        return document.querySelectorAll('mat-table.partition-table input.mat-input-element')[0];
       case 'experiment-stepper-design-decision-points-row1-target':
-        return document.querySelectorAll('mat-table.decision-point-table input.mat-mdc-input-element')[1];
+        return document.querySelectorAll('mat-table.partition-table input.mat-input-element')[1];
       case 'experiment-stepper-design-decision-points-row1-exclude-if-reached':
-        return document.querySelector('mat-table.decision-point-table input.mdc-checkbox__native-control');
-      case 'experiment-stepper-design-decision-points-row1-confirm-button':
-        return document.querySelectorAll('mat-table.decision-point-table button.row-action-btn')[0];
+        return document.querySelector('mat-table.partition-table input.mat-checkbox-input');
       case 'experiment-stepper-design-add-condition-button':
         return document.querySelector('form.experiment-design button.add-condition');
       case 'experiment-stepper-design-conditions-row1-condition':
-        return document.querySelectorAll('mat-table.condition-table input.mat-mdc-input-element')[0];
-      case 'experiment-stepper-design-conditions-row1-confirm':
-        return document.querySelectorAll('mat-table.condition-table button.row-action-btn')[0];
+        return document.querySelectorAll('mat-table.condition-table input.mat-input-element')[0];
       case 'experiment-stepper-design-conditions-row2-condition':
-        return document.querySelectorAll('mat-table.condition-table input.mat-mdc-input-element')[0];
-      case 'experiment-stepper-design-conditions-row2-confirm':
-        return document.querySelectorAll('mat-table.condition-table button.row-action-btn')[2];
+        return document.querySelectorAll('mat-table.condition-table input.mat-input-element')[3];
       case 'experiment-stepper-design-next-button':
-        return document.querySelectorAll('div.new-experiment-modal button.mat-mdc-raised-button')[4];
+        return document.querySelectorAll('div.new-experiment-modal button.mat-raised-button')[4];
 
       // Experiment Stepper - Participants Step
-      case 'experiment-stepper-participants-add-member-button':
-        return document.querySelector('form.experiment-participants button.add-member');
-      case 'experiment-stepper-participants-include-row1-type':
-        return document.querySelectorAll('mat-table.member-table div.mat-mdc-select-value')[0];
+      case 'experiment-stepper-participants-inclusion-criteria':
+        return document.querySelector('form.experiment-participants div.mat-select-value');
       case 'experiment-stepper-participants-next-button':
-        return document.querySelectorAll('div.new-experiment-modal button.mat-mdc-raised-button')[7];
+        return document.querySelectorAll('div.new-experiment-modal button.mat-raised-button')[7];
 
       // Experiment Stepper - Metrics Step
       case 'experiment-stepper-metrics-add-metric-button':
         return document.querySelector('form.metric-design button.add-metric');
       case 'experiment-stepper-metrics-metrics-row1-metric':
-        return document.querySelectorAll('mat-table.metric-table input.mat-mdc-input-element')[0];
+        return document.querySelectorAll('mat-table.metric-table input.mat-input-element')[0];
       case 'experiment-stepper-metrics-metrics-row1-statistic':
-        return document.querySelectorAll('mat-table.metric-table div.mat-mdc-select-value')[0];
+        return document.querySelectorAll('mat-table.metric-table div.mat-select-value')[0];
       case 'experiment-stepper-metrics-metrics-row1-display-name':
-        return document.querySelectorAll('mat-table.metric-table input.mat-mdc-input-element')[1];
+        return document.querySelectorAll('mat-table.metric-table input.mat-input-element')[1];
       case 'experiment-stepper-metrics-metrics-row2-metric':
-        return document.querySelectorAll('mat-table.metric-table input.mat-mdc-input-element')[2];
+        return document.querySelectorAll('mat-table.metric-table input.mat-input-element')[2];
       case 'experiment-stepper-metrics-metrics-row2-statistic':
-        return document.querySelectorAll('mat-table.metric-table div.mat-mdc-select-value')[1];
+        return document.querySelectorAll('mat-table.metric-table div.mat-select-value')[1];
       case 'experiment-stepper-metrics-metrics-row2-display-name':
-        return document.querySelectorAll('mat-table.metric-table input.mat-mdc-input-element')[3];
+        return document.querySelectorAll('mat-table.metric-table input.mat-input-element')[3];
       case 'experiment-stepper-metrics-next-button':
-        return document.querySelectorAll('div.new-experiment-modal button.mat-mdc-raised-button')[10];
+        return document.querySelectorAll('div.new-experiment-modal button.mat-raised-button')[10];
 
       // Experiment Stepper - Schedule Step
       case 'experiment-stepper-schedule-next-button':
-        return document.querySelectorAll('div.new-experiment-modal button.mat-mdc-raised-button')[13];
+        return document.querySelectorAll('div.new-experiment-modal button.mat-raised-button')[13];
 
       // Experiment Stepper - Post Rule Step
       case 'experiment-stepper-post-rule-post-rule':
-        return document.querySelectorAll('form.post-experiment-rule-form div.mat-mdc-select-value')[0];
+        return document.querySelectorAll('form.post-experiment-rule-form div.mat-select-value')[0];
       case 'experiment-stepper-post-rule-create-button':
-        return document.querySelectorAll('div.new-experiment-modal button.mat-mdc-raised-button')[16];
+        return document.querySelectorAll('div.new-experiment-modal button.mat-raised-button')[16];
 
       // Experiment Details - Overview Tab
       case 'experiment-details-overview-status':
@@ -94,13 +86,13 @@ if (window.self !== window.top) {
 
       // Experiment Details - Data Tab
       case 'experiment-details-data-tab':
-        return document.querySelectorAll('div.mat-mdc-tab-list div.mat-mdc-tab')[4];
+        return document.querySelectorAll('div.mat-tab-list div.mat-tab-label')[4];
 
       // Change Experiment Status Modal
       case 'change-experiment-status-modal-new-status':
-        return document.querySelectorAll('form.experiment-status-form div.mat-mdc-select-value')[1];
+        return document.querySelectorAll('form.experiment-status-form div.mat-select-value')[1];
       case 'change-experiment-status-modal-save-button':
-        return document.querySelectorAll('div.button-container button.mat-mdc-raised-button')[1];
+        return document.querySelectorAll('div.button-container button.mat-raised-button')[1];
     }
     console.error(`Error: The element ID "${id}" is not valid.`);
     return null;
@@ -118,7 +110,7 @@ if (window.self !== window.top) {
       activeWindowEvent.target.removeEventListener(activeWindowEvent.type, activeWindowEvent.callback, true);
       activeWindowEvent = null;
     }
-  };
+  }
 
   const addWindowEvent = (target, type, callback) => {
     removeActiveWindowEvent();
@@ -130,50 +122,46 @@ if (window.self !== window.top) {
     activeWindowEvent = {
       target,
       type,
-      callback,
+      callback
     };
-  };
+  }
 
   const closeOpenedModals = () => {
-    const modalButtonsSelector = 'div.cdk-overlay-pane button.mat-mdc-raised-button';
-    const modalCloseButton = Array.from(document.querySelectorAll(modalButtonsSelector)).find(
-      (elem) => elem.innerText.toLowerCase() === 'close'
-    );
+    const modalButtonsSelector = 'div.cdk-overlay-pane button.mat-raised-button';
+    const modalCloseButton = Array.from(document.querySelectorAll(modalButtonsSelector)).find((elem) => elem.innerText.toLowerCase() === 'close');
     if (modalCloseButton) {
       modalCloseButton.click();
-      const confirmCloseButton = Array.from(document.querySelectorAll(modalButtonsSelector)).find(
-        (elem) => elem.innerText.toLowerCase() === 'yes'
-      );
+      const confirmCloseButton = Array.from(document.querySelectorAll(modalButtonsSelector)).find((elem) => elem.innerText.toLowerCase() === 'yes');
       if (confirmCloseButton) {
         confirmCloseButton.click();
       }
     }
-  };
+  }
 
   // A set of functions that do more than triggering an event
   const initCallFunction = {
     'set-zoom-level': (args) => {
       document.body.style.zoom = args[0];
     },
-    logout: () => {
+    'logout': () => {
       closeOpenedModals();
       const signOutButton = getElementById('signout-button');
       if (signOutButton) {
         signOutButton.click();
+      } else if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+        gapi.auth2.getAuthInstance().signOut();
       }
     },
     'on-upgrade-tab-click': () => {
-      if (!getElementById('upgrade-logo-link')) {
+      if (!gapi.auth2.getAuthInstance().isSignedIn.get()) {
         return closeOpenedModals();
       }
       const experimentsTab = getElementById('experiments-tab');
-      if (experimentsTab) {
-        experimentsTab.click();
-      }
+      experimentsTab.click();
     },
     'remove-active-window-event': () => {
       removeActiveWindowEvent();
-    },
+    }
   };
 
   // Used to call a function or trigger event on element
@@ -205,16 +193,14 @@ if (window.self !== window.top) {
     'on-experiment-click': (args, onNextCall) => {
       const experimentName = args[0];
       const onWindowClick = (event) => {
-        const elem = Array.from(
-          document.querySelectorAll('div.experiment-list-table-container a.experiment-name')
-        ).find((elem) => elem.innerText === experimentName);
+        const elem = Array.from(document.querySelectorAll('div.experiment-list-table-container a.experiment-name')).find((elem) => elem.innerText === experimentName);
         if (elem && elem.contains(event.target)) {
           removeActiveWindowEvent();
           onNextCall();
         }
       };
       addWindowEvent(window, 'click', onWindowClick);
-    },
+    }
   };
 
   // Used to detect event on element
