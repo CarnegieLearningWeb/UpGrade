ce88acb18355351c56286c275b96cd8e5b5367ce
Zack Lee <zlee@carnegielearning.com>
2025-09-23 03:06:49 +0900

    Fix the frontend settings to work on /upgrade path for the demo app

--- frontend/projects/server/demo-server.js
+++ frontend/projects/server/demo-server.js
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 4200;
 const app = express();
 
 app.use(compression());
-app.use(CONTEXT, express.static(path.resolve(__dirname, '../../dist')));
-app.use('*', express.static(path.resolve(__dirname, '../../dist/upgrade')));
+app.use(CONTEXT, express.static(path.resolve(__dirname, '../../dist/upgrade/browser')));
+app.use('*', express.static(path.resolve(__dirname, '../../dist/upgrade/browser')));
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}${CONTEXT}`));
