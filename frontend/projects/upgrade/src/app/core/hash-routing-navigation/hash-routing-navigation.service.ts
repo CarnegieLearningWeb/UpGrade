d93c17b72a8e653243af198e1c98164ec410fd90
doswalt <doswalt@carnegielearning.com>
2025-09-25 11:24:38 -0400

    manually tell router to navigate when hashchangeevent fires

--- frontend/projects/upgrade/src/app/core/hash-routing-navigation/hash-routing-navigation.service.ts
+++ frontend/projects/upgrade/src/app/core/hash-routing-navigation/hash-routing-navigation.service.ts
@@ -6,8 +6,6 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root',
 })
 export class HashRoutingNavigationService {
-  private currentUrl = '';
-
   constructor(private readonly router: Router) {}
 
   public initializeHashRouting(): void {
@@ -15,21 +13,8 @@ export class HashRoutingNavigationService {
       return;
     }
 
-    this.currentUrl = window.location.hash.substring(1);
-
-    window.addEventListener('hashchange', (event) => {
-      const newUrl = (event as any).newURL;
-      const oldUrl = (event as any).oldURL;
-
-      if (!newUrl || !oldUrl) return;
-
-      const newHashPath = newUrl.split('#')[1];
-      const oldHashPath = oldUrl.split('#')[1];
-
-      if (newHashPath !== oldHashPath && newHashPath !== this.currentUrl) {
-        this.currentUrl = newHashPath;
-        this.router.navigateByUrl(newHashPath);
-      }
+    window.addEventListener('hashchange', (event: any) => {
+      this.router.navigateByUrl(event);
     });
   }
 }
