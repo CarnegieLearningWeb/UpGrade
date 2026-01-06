63f1a54be0ee938b4fc96ad13f72281228ab4486
danoswaltCL <97542869+danoswaltCL@users.noreply.github.com>
2026-01-06 15:02:13 -0500

    move old enrollment data page components into new structure (#2812)
    * move old enrollment data page components into new structure
    
    * remove unused import after restructure
    
    * add import needed after restructure

---
+++ frontend/projects/upgrade/src/app/features/dashboard/experiments/pages/experiment-details-page/experiment-details-page-content/experiment-enrollment-data-section-card/enrollment-condition-table/enrollment-condition-table.component.ts
@@ -0,0 +1,97 @@
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import {
+  ASSIGNMENT_UNIT,
+  ExperimentVM,
+  EnrollmentByConditionOrPartitionData,
+} from '../../../../../../../../core/experiments/store/experiments.model';
+import { ExperimentService } from '../../../../../../../../core/experiments/experiments.service';
+import { Subscription } from 'rxjs';
+import { TranslateModule } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
+import { EnrollmentConditionExpandableRowComponent } from './enrollment-condition-expandable-row/enrollment-condition-expandable-row.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-enrollment-condition-table',
+  templateUrl: './enrollment-condition-table.component.html',
+  styleUrls: ['./enrollment-condition-table.component.scss'],
+  imports: [CommonModule, TranslateModule, EnrollmentConditionExpandableRowComponent, MatProgressSpinnerModule],
+})
+export class EnrollmentConditionTableComponent implements OnChanges, OnInit, OnDestroy {
+  @Input() experiment: ExperimentVM;
+  experimentData: any[] = [];
+  commonColumns = ['expandIcon', 'condition', 'weight', 'userEnrolled'];
+  displayedColumns: string[] = [];
+  isStatLoading = true;
+  experimentStateSub: Subscription;
+
+  constructor(private experimentService: ExperimentService) {}
+
+  ngOnChanges() {
+    if (this.experiment.assignmentUnit === ASSIGNMENT_UNIT.INDIVIDUAL) {
+      this.displayedColumns = this.commonColumns;
+    } else {
+      this.displayedColumns = [...this.commonColumns, 'groupEnrolled'];
+    }
+  }
+
+  ngOnInit() {
+    this.experimentStateSub = this.experimentService.experimentStatById$(this.experiment.id).subscribe((stat) => {
+      this.experimentData = [];
+      if (stat && stat.conditions) {
+        this.isStatLoading = false;
+        stat.conditions.forEach((condition) => {
+          const conditionObj: EnrollmentByConditionOrPartitionData = {
+            condition: this.getConditionData(condition.id, 'conditionCode'),
+            weight: this.getConditionData(condition.id, 'assignmentWeight'),
+            userEnrolled: condition.users,
+            groupEnrolled: condition.groups,
+          };
+          let experimentObj: any = {
+            data: conditionObj,
+          };
+
+          const partitions = [];
+          condition.partitions.forEach((partition) => {
+            const partitionObj: EnrollmentByConditionOrPartitionData = {
+              experimentPoint: this.getPartitionData(partition.id, 'site'),
+              experimentId: this.getPartitionData(partition.id, 'target') || '',
+              userEnrolled: partition.users,
+              groupEnrolled: partition.groups,
+            };
+            partitions.push({
+              data: partitionObj,
+            });
+          });
+          experimentObj = {
+            ...experimentObj,
+            partitions,
+          };
+          this.experimentData.push(experimentObj);
+        });
+      }
+    });
+  }
+
+  getPartitionData(partitionId: string, key: string) {
+    return this.experiment.partitions.reduce(
+      (acc, partition) => (partition.id === partitionId ? (acc = partition[key]) : acc),
+      null
+    );
+  }
+
+  getConditionData(conditionId: string, key: string) {
+    return this.experiment.conditions.reduce(
+      (acc, condition) => (condition.id === conditionId ? (acc = condition[key]) : acc),
+      null
+    );
+  }
+
+  ngOnDestroy() {
+    this.experimentStateSub.unsubscribe();
+  }
+
+  get AssignmentUnit() {
+    return ASSIGNMENT_UNIT;
+  }
+}
