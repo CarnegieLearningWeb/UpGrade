189859684ce905be638907a9fd39e0faa2c10384
Benjamin Blanchard <bblanchard@carnegielearning.com>
2026-04-23 13:08:35 -0400

    fix lint

--- packages/frontend/projects/upgrade/setup-jest.ts
+++ packages/frontend/projects/upgrade/setup-jest.ts
@@ -6,6 +6,7 @@ import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 setupZoneTestEnv();
 Object.defineProperty(global, 'crypto', {
   value: {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
     randomUUID: () => require('crypto').randomUUID(),
   },
 });
