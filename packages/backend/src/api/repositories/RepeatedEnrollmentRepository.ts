3476f99f6fae14ab8683adbc4980a6a85bb4a73e
Benjamin Blanchard <bblanchard@carnegielearning.com>
2026-06-26 16:29:30 -0400

    get results for each experiment, rather than all

--- packages/backend/src/api/repositories/RepeatedEnrollmentRepository.ts
+++ packages/backend/src/api/repositories/RepeatedEnrollmentRepository.ts
@@ -6,23 +6,24 @@ import repositoryError from './utils/repositoryError';
 
 export interface RepeatedEnrollmentDataCount {
   userId: string;
-  decisionPointId: string;
+  experimentId: string;
   count: number;
 }
 @EntityRepository(RepeatedEnrollment)
 export class RepeatedEnrollmentRepository extends Repository<RepeatedEnrollment> {
   public async getRepeatedEnrollmentCount(
     userId: string,
-    decisionPointsIds: string[],
+    experimentIds: string[],
     logger: UpgradeLogger
   ): Promise<RepeatedEnrollmentDataCount[]> {
     const result = await this.createQueryBuilder('repeatedEnrollment')
-      .select(['ie.userId as "userId"', 'ie.partitionId as "decisionPointId"'])
+      .select(['ie.userId as "userId"', 'ie.experimentId as "experimentId"'])
       .addSelect('COUNT(*) as count')
       .leftJoin('repeatedEnrollment.individualEnrollment', 'ie')
+      .leftJoin('ie.experiment', 'experiment')
       .where('ie.userId = :userId', { userId })
-      .andWhere('ie.partitionId IN (:...decisionPointsIds)', { decisionPointsIds })
-      .groupBy('ie.userId , ie.partitionId , ie.id')
+      .andWhere('ie.experimentId IN (:...experimentIds)', { experimentIds })
+      .groupBy('ie.userId , ie.experimentId , ie.id')
       .getRawMany()
       .catch((errorMsg: any) => {
         const errorMsgString = repositoryError(
