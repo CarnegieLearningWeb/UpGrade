02ae0eff09ffc33a642b6d6b3ef6f481d229a226
Zack Lee <zlee@carnegielearning.com>
2026-08-12 15:53:39 -0400

    Document feature flag ID search in Swagger

--- packages/backend/src/api/controllers/FeatureFlagController.ts
+++ packages/backend/src/api/controllers/FeatureFlagController.ts
@@ -243,7 +243,7 @@ export class FeatureFlagsController {
    *                properties:
    *                  key:
    *                    type: string
-   *                    enum: [all, name, key, status, tag, context, listValue]
+   *                    enum: [all, name, key, status, tag, context, id, listValue]
    *                  string:
    *                    type: string
    *               sortParams:
